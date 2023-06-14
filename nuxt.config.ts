553f15d34b6bc9a9b76e4ffd2c4abcf62ff32d0a
ipeglin <philip@eglin.no>
2023-06-14 18:13:42 +0200

    fix(env): fixed incorrect database environment variable name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     public: {
       FB_API_KEY: process.env.NUXT_FB_API_KEY,
       FB_AUTH_DOMAIN: process.env.NUXT_FB_AUTH_DOMAIN,
-      FB_DB_URL: process.env.NUXT_FB_DB_URL,
+      FB_DB_URL: process.env.NUXT_FB_DATABASE_URL,
       FB_PROJECT_ID: process.env.NUXT_FB_PROJECT_ID,
       FB_STORAGE_BUCKET: process.env.NUXT_FB_STORAGE_BUCKET,
       FB_MESSAGING_SENDER_ID: process.env.NUXT_FB_MESSAGING_SENDER_ID,
