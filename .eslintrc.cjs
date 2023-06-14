f7f6611386479afa6019020ccbc040aa8ce63e6e
ipeglin <philip@eglin.no>
2023-06-14 19:52:20 +0200

    feature(linting): added linting rules for code

---
+++ .eslintrc.cjs
@@ -0,0 +1,77 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [
+    {
+      files: ['**/*.vue'],
+      processor: 'vue/.vue',
+    },
+  ],
+  plugins: [],
+  rules: {
+    'vue/no-unused-vars': 'error',
+    'arrow-spacing': ["error", { "before": true, "after": true }],
+    'brace-style': "error",
+    'camelcase': "error",
+    "comma-dangle": ["error", {
+      "arrays": "always-multiline",
+      "objects": "always-multiline",
+      "imports": "only-multiline",
+      "exports": "only-multiline",
+      "functions": "never",
+    }],
+    'comma-spacing': ["error", { "before": false, "after": true }],
+    'comma-style': ["error", "last"],
+    'dot-notation': ["error", { "allowKeywords": false }],
+    'eqeqeq': ["error", "smart"],
+    'func-call-spacing': ["error", "never"],
+    'key-spacing': ["error", { "beforeColon": false, "afterColon": true, "mode": "strict" }],
+    'keyword-spacing': ["error", { "before": true, "after": false}],
+    "max-len": ["error", {
+      "code": 80,
+      "template": 80,
+      "tabWidth": 2,
+      "comments": 80,
+      "ignorePattern": "",
+      "ignoreComments": false,
+      "ignoreTrailingComments": false,
+      "ignoreUrls": false,
+      "ignoreStrings": false,
+      "ignoreTemplateLiterals": false,
+      "ignoreRegExpLiterals": false,
+      "ignoreHTMLAttributeValues": false,
+      "ignoreHTMLTextContents": false,
+    }],
+    'no-constant-condition': "error",
+    "no-irregular-whitespace": ["error", {
+      "skipStrings": true,
+      "skipComments": false,
+      "skipRegExps": false,
+      "skipTemplates": false,
+      "skipHTMLAttributeValues": false,
+      "skipHTMLTextContents": false
+    }],
+    'no-loss-of-precision': "error",
+    'no-sparse-arrays': "error",
+    'no-useless-concat': "error",
+    'object-curly-newline': ["error", { "minProperties": 2 }],
+    'object-curly-spacing': ["error", "always", {"arraysInObjects": true, "objectsInObjects": true }],
+    'object-property-newline': ["error", { "allowAllPropertiesOnSameLine": true }],
+    'prefer-template': "error",
+    'quote-props': ["error", "as-needed"],
+    "space-in-parens": "error",
+    'template-curly-spacing': ["error", "always"],
+    'quotes': ["error", "single", { "avoidEscape": true, "allowTemplateLiterals": true }]
+  },
+};
