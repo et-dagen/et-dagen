3d1d5c53c2439aceac8f1620c259c6b5616e0441
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-06-20 02:53:19 +0200

    feat(middleware): auth middleware

---
+++ middleware/auth.global.ts
@@ -0,0 +1,16 @@
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  // route is not protected
+  if (!to.meta.protected) return
+
+  const authStore = useAuthStore()
+
+  // user is logged in
+  if (authStore.isLoggedIn) return
+
+  // user is not logged in
+  authStore.pendingRoute = to.fullPath
+
+  // for some reason using navigateTo('/') resets the store value on the initial page load
+  // return navigateTo('/')
+  return useRouter().push({ path: '/' })
+})
