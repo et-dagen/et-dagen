d97f51abb306255835f2f1b20a66670842085d12
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-06-20 02:44:10 +0200

    feat(pages): sign in button
    - Add temporary signin button

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    <VBtn color="success">Hello world!</VBtn>
+    <VBtn color="success" @click="signIn">Sign in</VBtn>
+    {{ user }}
   </div>
 </template>
+
+<script setup lang="ts">
+  import { storeToRefs } from 'pinia'
+
+  const authStore = useAuthStore()
+  const { user } = storeToRefs(authStore)
+
+  const signIn = () => signinUser('email', 'password')
+</script>
