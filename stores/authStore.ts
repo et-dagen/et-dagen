fdd27ff337f711c8a7b2892e6edcb61e49187dd5
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-06-22 01:51:28 +0200

    fix(stores): remove pending-route

--- stores/authStore.ts
+++ stores/authStore.ts
@@ -2,14 +2,12 @@ import type { User } from '~~/models/User'
 
 interface State {
   user: User | null
-  pendingRoute: string | null
 }
 
 export const useAuthStore = defineStore('auth', {
   state: (): State => {
     return {
       user: null,
-      pendingRoute: null,
     }
   },
   getters: {
