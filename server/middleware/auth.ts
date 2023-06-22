de3614639e218eded60067c1b5205f5e9a4b5e53
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-06-22 02:00:06 +0200

    feat(server): auth middleware
    - Add server middleware to validate idToken read from token cookie

---
+++ server/middleware/auth.ts
@@ -0,0 +1,15 @@
+import { auth } from '~~/server/lib/firebase'
+
+export default defineEventHandler(async (event) => {
+  // get idToken from cookie
+  const idToken = getCookie(event, 'token')
+  if (!idToken) return
+
+  try {
+    // check validity of idToken
+    const decodedToken = await auth.verifyIdToken(idToken)
+    event.context.auth = decodedToken
+  } catch (error) {
+    // idToken invalid, the user is not authenticated
+  }
+})
