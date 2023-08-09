71a32ada5c8d4bbe191e4639d39c957bccea9109
ipeglin <philip@eglin.no>
2023-08-09 15:47:14 +0200

    feat: added ci script for deploying app to Digital Ocean. NB! Missing deployment code. See issue #36

---
+++ ci.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Exit on any non-zero status.
+set -e
+
+# Define environment variables
+export NODE_ENV=production
+
+# Install dependencies
+echo "Installing dependencies..."
+npm install
+
+# Run linting and testing script
+./lint-test.sh
+
+# TODO: #36 Configure deployment script to Digital Ocean
+# Deploy to Digital Ocean
+# echo "Deploying to Digital Ocean..."
+
+echo "Deployment completed successfully!"
