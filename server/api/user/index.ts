b6f9f9c2f22847dc0b02af6f3f74533031de194d
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-08-14 21:35:49 +0200

    feat(api): get user endpoint

---
+++ server/api/user/index.ts
@@ -0,0 +1,35 @@
+export default defineEventHandler(async (event) => {
+  const { decodedToken } = event.context
+
+  // user is not authenticated
+  if (!decodedToken) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'User not authenticated',
+    })
+  }
+
+  // reference to users
+  const usersRef = db.ref('users')
+
+  try {
+    // get user data
+    const snapshot = await usersRef
+      .orderByKey()
+      .equalTo(decodedToken.sub)
+      .once('value')
+
+    // get user object
+    const data = snapshot.val()
+    if (!data)
+      throw createError({
+        statusCode: 404,
+        statusMessage: 'User not found in database',
+      })
+
+    // return user data
+    return data
+  } catch (error) {
+    throw error
+  }
+})
