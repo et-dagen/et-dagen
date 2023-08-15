5b9b656c561d4020776f82b50ffe4cb69765b03e
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-08-16 00:06:04 +0200

    feat: update auth middleware
    - Populating pinia store server side removes the need for some of the old code.

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,21 +1,10 @@
 export default defineNuxtRouteMiddleware(async (to) => {
-  // skip middleware if app is hydrating
-  const nuxtApp = useNuxtApp()
-  if (nuxtApp.isHydrating) return
-
   // route is protected
   if (!to.meta.protected) return
 
-  // makes sure that the token cookie is included
-  // in the initial request headers
-  const headers = useRequestHeaders(['cookie'])
-
   // when navigating client side the auth state is
   // collected from the auth store
-  const isLoggedIn = process.client
-    ? useAuthStore().isLoggedIn
-    : // check auth server side
-      await $fetch('/api/auth', { headers })
+  const isLoggedIn = useAuthStore().isLoggedIn
 
   // user is authenticated
   if (isLoggedIn) return
