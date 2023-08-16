2a7930b5397c1a7562366befef486aba9300bcad
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-08-16 13:42:46 +0200

    chore(utils): rename format-user

--- server/utils/formatFirebaseUser.ts
+++ server/utils/formatFirebaseUser.ts
@@ -1,7 +1,8 @@
 import type { User as FirebaseUser } from 'firebase/auth'
+import type { UserRecord } from 'firebase-admin/auth'
 import type { TokenData } from '@/models/User'
 
-export default (user: FirebaseUser): TokenData => {
+export default (user: FirebaseUser | UserRecord): TokenData => {
   return {
     uid: user.uid,
     email: user.email || '',
