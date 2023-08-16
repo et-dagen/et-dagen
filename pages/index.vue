3f22d8a4f931da55d7b360046e0c76d218f2e8a7
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-08-16 14:35:41 +0200

    temp(pages): api tests

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <!-- Sign in/out buttons -->
-    <VBtn v-if="!authStore.isLoggedIn" color="success" @click="signIn">{{
-      $t('sign_in')
-    }}</VBtn>
+    <VBtn v-if="!authStore.isLoggedIn" color="success" @click="signIn">
+      Logg inn
+    </VBtn>
     <VBtn v-else color="error" @click="signOut">Sign out</VBtn>
-    {{ user }}
+
+    <p>User from store: {{ user }}</p>
+    <p>User from api: {{ data }} {{ error }}</p>
+
+    <VBtn color="info" @click="createUser">Create user</VBtn>
+    <VBtn color="error" @click="deleteUser">Delete user</VBtn>
+    <VBtn color="success" @click="refresh">Refresh user data</VBtn>
   </div>
 </template>
 
@@ -15,6 +21,39 @@
   const authStore = useAuthStore()
   const { user } = storeToRefs(authStore)
 
-  const signIn = () => signinUser('email', 'password')
+  const signIn = () => signinUser('aasmund@cot.as', 'abc123+A')
   const signOut = () => signoutUser()
+
+  const { data, error, refresh } = await useFetch('/api/user', {
+    // add scope to get all users
+    params: { scope: 'all' },
+  })
+
+  const createUser = async () => {
+    try {
+      const data = await $fetch('/api/user', {
+        method: 'POST',
+        body: {
+          // sub: 'mogus',
+          // accessLevel: [],
+          studyProgram: 'data',
+        },
+      })
+    } catch (error) {
+      // handle error
+    }
+  }
+
+  const deleteUser = async () => {
+    try {
+      const data = await $fetch('/api/user', {
+        method: 'DELETE',
+        body: {
+          sub: 'mogus',
+        },
+      })
+    } catch (error) {
+      // handle error
+    }
+  }
 </script>
