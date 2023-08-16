ba441b67fb79dd9d966d1f66e70bc438ada37f1c
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-08-16 02:45:40 +0200

    feat(api): create user endpoint

---
+++ server/api/user/index.post.ts
@@ -0,0 +1,37 @@
+// POST /api/user
+// endpoint for creating or updating a user in the database
+
+export default defineEventHandler(async (event) => {
+  const { decodedToken, user } = event.context
+
+  // user is not authenticated
+  if (!decodedToken)
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'User not authenticated',
+    })
+
+  // get request body
+  let { sub, accessLevel, studyProgram } = await readBody(event)
+
+  if (!studyProgram)
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'Missing study program',
+    })
+
+  // only admins can modify user access levels
+  if (!hasAccessLevel(user, 'admin') || !sub || !accessLevel) {
+    sub = decodedToken.sub
+    accessLevel = []
+  }
+
+  // reference to users
+  const usersRef = db.ref('users')
+
+  // add or update user in database
+  usersRef.child(sub).set({
+    accessLevel,
+    studyProgram,
+  })
+})
