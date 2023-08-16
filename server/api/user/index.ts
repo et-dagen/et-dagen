33a8ccb498a63f4c4693ef85b953cf4c14e9ddff
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-08-16 02:19:03 +0200

    feat: update errors

--- server/api/user/index.ts
+++ server/api/user/index.ts
@@ -2,13 +2,19 @@
 // endpoint for fetching a users data from database
 
 export default defineEventHandler(async (event) => {
-  const { user } = event.context
-  
+  const { decodedToken, user } = event.context
+
   // user is not authenticated
-  if (!user)
+  if (!decodedToken)
     throw createError({
       statusCode: 401,
       statusMessage: 'User not authenticated',
+    })
+
+  if (!user)
+    throw createError({
+      statusCode: 404,
+      statusMessage: 'User data not found',
     })
 
   // return user data
