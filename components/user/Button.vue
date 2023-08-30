41d41132c540dbf6439774e65a97c03ea504ac40
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-08-30 17:26:44 +0200

    feat: user buttons
    Add user menu with quick access user actions and user button to sign out or navigate to sign in page.

---
+++ components/user/Button.vue
@@ -0,0 +1,14 @@
+<template>
+  <VBtn
+    rounded="lg"
+    :prepend-icon="auth.isLoggedIn ? 'mdi-logout' : 'mdi-login'"
+    variant="text"
+    @click="auth.isLoggedIn ? signoutUser() : navigateTo('/user/login')"
+  >
+    {{ auth.isLoggedIn ? $t('user.sign_out') : $t('user.sign_in') }}
+  </VBtn>
+</template>
+
+<script setup lang="ts">
+  const auth = useAuthStore()
+</script>
