20e3587067c4f38e03f297ba1ef058ba37ac0a73
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-08-30 23:56:56 +0200

    feat: set token cookie server side

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,8 +1,21 @@
-export default defineEventHandler(async (event) => {
-  // get idToken from cookie
-  const idToken = getCookie(event, 'token')
+// 60sec * 60min * 24h = 1 day in seconds
+const dayInSeconds = 60 * 60 * 24
 
+export default defineEventHandler(async (event) => {
+  // get idToken from Authorization header or cookie
+  let idToken = getHeader(event, 'Authorization') ?? getCookie(event, '_token')
+  console.log(idToken)
+  // no idToken
   if (!idToken) return
+
+  // set new token cookie on response
+  if (idToken.includes('Bearer')) {
+    idToken = idToken.split('Bearer ')[1]
+    setCookie(event, '_token', idToken, {
+      httpOnly: true,
+      maxAge: dayInSeconds * 1,
+    })
+  }
 
   try {
     // check validity of idToken
