c229b65e77129ac5e44a4414a48c3853ee547907
Joresund <johanstore2002@gmail.com>
2023-08-31 13:07:24 +0200

    feature: added custom error page

---
+++ components/error/errorCardMobile.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="text-center border-1px px-10 py-5"
+    style="
+      border: 1px solid black;
+      border-radius: 10px;
+      position: fixed;
+      width: 350px;
+      max-width: 90vw;
+    "
+  >
+    <h1 class="text-h2 font-weight-bold" style="color: orange">Oops!</h1>
+    <h2 class="text-h4">Error {{ error.statusCode }}</h2>
+    <p class="text-h6" style="word-break: break-word; max-width: 100%">
+      {{ error.message }}
+    </p>
+    <NuxtLink to="/" class="text-h6">Tilbake til forsiden</NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+  defineProps(['error'])
+</script>
---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="d-flex h-screen align-center justify-center">
+    <ErrorCard v-if="!mobile" :error="error" />
+    <ErrorCardMobile v-else :error="error" />
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps({
+    error: Object(),
+  })
+
+  console.log(props)
+
+  const { mobile } = useDisplay()
+</script>
