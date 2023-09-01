5536be8b3d133d04c33b185db8709ea8ab292be1
Reidar Arne Eidsvik Nerheim <raener12@gmail.com>
2023-09-01 17:38:23 +0200

    fix: added some comments

--- components/locale-switcher.vue
+++ components/locale-switcher.vue
@@ -1,8 +1,10 @@
 <script setup>
   const { locale, locales } = useI18n()
+  // generates URLs to switch between locales
   const switchLocalePath = useSwitchLocalePath()
 
   const availableLocales = computed(() => {
+    // returns a list of locales, exluding current locale
     return locales.value.filter((i) => i.code !== locale.value)
   })
 </script>
@@ -14,12 +16,19 @@
     <!-- v-model="selectedLocale" = binds <select> to "selectedLocale"-->
     <!-- @change="changeLocale" = listen "change" event, call "changeLocale" -->
     <select v-model="selectedLocale" @change="changeLocale">
+      <!--
+        Nuxtlink: component for routing withinn app
+        v-for: iterate over all locales in availableLocales
+        :key:  each locale is assigned a unique key
+        :to    the "to" vaule of each component is set to its URL
+      -->
       <NuxtLink
         v-for="locale in availableLocales"
         :key="locale.code"
         :to="switchLocalePath(locale.code)"
       >
-        {{ locale.name }}
+        {{ locale.name
+        }}<!-- Display available locales-->
       </NuxtLink>
     </select>
   </div>
