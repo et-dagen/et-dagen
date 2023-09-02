c7726442d96f717008f92242a2227b64eae2d99b
Reidar Arne Eidsvik Nerheim <raener12@gmail.com>
2023-09-02 13:56:52 +0200

    fix: removed comments, changed div class => VMenu

--- components/locale-switcher.vue
+++ components/locale-switcher.vue
@@ -11,7 +11,12 @@
 
 <!-- template defines the visual structure of the component -->
 <template>
-  <div class="locale-switcher">
+  <VMenu transition="scale-transition">
+    <!-- activator btn -->
+    <template #activator="{ props }">
+      <VBtn v-bind="props" icon="mdi-translate-outline" />
+    </template>
+
     <!-- template contains a <select> element -->
     <!-- v-model="selectedLocale" = binds <select> to "selectedLocale"-->
     <!-- @change="changeLocale" = listen "change" event, call "changeLocale" -->
@@ -31,34 +36,5 @@
         }}<!-- Display available locales-->
       </NuxtLink>
     </select>
-  </div>
+  </VMenu>
 </template>
-
-<!--
-<select v-model="selectedLocale" @change="changeLocale"> 
-    <option v-for="(locale, index) in availableLocales" :key="index" :value="locale">
-    {{ locale }}
-    </option>
-</select>
--->
-
-<!--
-<script>
-    export default {
-        //data() function defines initial data properties
-        data() {
-            return {
-                availableLocales:['NO','EN','ENU'], //add locales here
-                selectedLocale:'NO' //default locale
-            };
-        },
-        //method is a section with functions that the component has access to
-        methods:{
-            //changeLocale() function changes the locale when called
-            changeLocale(){
-                //logic to change locale
-            }
-        }
-    };
-</script>
--->
