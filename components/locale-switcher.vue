e37772bb677827729559233194c39654050794af
Reidar Arne Eidsvik Nerheim <raener12@gmail.com>
2023-09-02 14:29:46 +0200

    feat: added tooltip to btn

--- components/locale-switcher.vue
+++ components/locale-switcher.vue
@@ -11,30 +11,42 @@
 
 <!-- template defines the visual structure of the component -->
 <template>
-  <VMenu transition="scale-transition">
-    <!-- activator btn -->
-    <template #activator="{ props }">
-      <VBtn v-bind="props" icon="mdi-translate-outline" />
-    </template>
+  <div class="locale-switcher">
+    <VMenu transition="scale-transition">
+      <!-- btn to activate menu -->
+      <template #v-slot:activator="{ props: menu }">
+        <v-tooltip location="top">
+          <template #activator="{ props: tooltip }">
+            <VBtn
+              :ripple="true"
+              v-bind="mergeProps(menu, tooltip)"
+              icon="mdi-translate-outline"
+            />
+          </template>
+          <!-- Should chnge this to be dependent on current language -->
+          <span>Change language</span>
+        </v-tooltip>
+      </template>
 
-    <!-- template contains a <select> element -->
-    <!-- v-model="selectedLocale" = binds <select> to "selectedLocale"-->
-    <!-- @change="changeLocale" = listen "change" event, call "changeLocale" -->
-    <select v-model="selectedLocale" @change="changeLocale">
-      <!--
-        Nuxtlink: component for routing withinn app
-        v-for: iterate over all locales in availableLocales
-        :key:  each locale is assigned a unique key
-        :to    the "to" vaule of each component is set to its URL
-      -->
-      <NuxtLink
-        v-for="locale in availableLocales"
-        :key="locale.code"
-        :to="switchLocalePath(locale.code)"
-      >
-        {{ locale.name
-        }}<!-- Display available locales-->
-      </NuxtLink>
-    </select>
-  </VMenu>
+      <!-- template contains a <select> element -->
+      <!-- v-model="selectedLocale" = binds <select> to "selectedLocale"-->
+      <!-- @change="changeLocale" = listen "change" event, call"changeLocale"-->
+      <select v-model="selectedLocale" @change="changeLocale">
+        <!--
+            Nuxtlink: component for routing withinn app
+            v-for: iterate over all locales in availableLocales
+            :key:  each locale is assigned a unique key
+            :to    the "to" vaule of each component is set to its URL
+        -->
+        <NuxtLink
+          v-for="locale in availableLocales"
+          :key="locale.code"
+          :to="switchLocalePath(locale.code)"
+        >
+          {{ locale.name
+          }}<!-- Display available locales-->
+        </NuxtLink>
+      </select>
+    </VMenu>
+  </div>
 </template>
