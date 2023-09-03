f2697ed50726638c0f6b5ea8ceac5bb8b64537de
ipeglin <philip@eglin.no>
2023-09-03 16:16:39 +0200

    refactor: using env variables provided in RuntimeConfig instead of hard coding string values

--- server/utils/firebase.ts
+++ server/utils/firebase.ts
@@ -3,11 +3,12 @@ import { getAuth } from 'firebase-admin/auth'
 import { getDatabase } from 'firebase-admin/database'
 
 let firebaseAdminApp: App
+const config = useRuntimeConfig()
 
 if (!getApps().length) {
   firebaseAdminApp = initializeApp({
-    credential: cert('firebase-admin.key.json'),
-    databaseURL: 'https://etdagen-d1f82.firebaseio.com',
+    credential: cert(config.GOOGLE_APPLICATION_CREDENTIALS),
+    databaseURL: config.public.FB_DB_URL,
   })
 } else {
   firebaseAdminApp = getApp()
