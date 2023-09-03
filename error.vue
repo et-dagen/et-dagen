17dc42a21846f75072214c17c6dd33ee7ce18a69
Joresund <johanstore2002@gmail.com>
2023-09-03 13:19:50 +0200

    refactor: restructured custom error pages

--- error.vue
+++ error.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="d-flex h-screen align-center justify-center">
-    <ErrorCard v-if="!mobile" :error="error" />
-    <ErrorCardMobile v-else :error="error" />
+    <v-sheet class="text-center">
+      <h1 class="text-orange text-h1 font-weight-bold pb-3">Oh no!</h1>
+      <h4 class="text-h4 pb-1">Error {{ props.error.statusCode }}</h4>
+      <h5 class="text-h5 pb-2" style="max-width: 90vw">
+        {{ props.error.message }}
+      </h5>
+      <v-btn @click="handleError">Return to frontpage</v-btn>
+    </v-sheet>
   </div>
 </template>
 
@@ -10,7 +16,8 @@
     error: Object(),
   })
 
-  console.log(props)
-
-  const { mobile } = useDisplay()
+  const handleError = () =>
+    clearError({
+      redirect: '/',
+    })
 </script>
---
+++ pages/[...slug].vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="d-flex h-screen align-center justify-center">
+    <v-sheet class="text-center">
+      <h1 class="text-h1 pb-10">404</h1>
+      <h5 class="text-h5 py-2" style="max-width: 90vw">
+        Denne siden finnes ikke: /{{ $route.params.slug[0] }}
+      </h5>
+      <v-btn @click="navigateTo('/')">Tilbake til forsiden</v-btn>
+    </v-sheet>
+  </div>
+</template>
