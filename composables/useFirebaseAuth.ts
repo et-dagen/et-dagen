d9f487779e041af553070b68911febd0ad7a8d3d
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-09-04 23:11:20 +0200

    feat: update register user composable

--- composables/useFirebaseAuth.ts
+++ composables/useFirebaseAuth.ts
@@ -3,19 +3,55 @@ import {
   getAuth,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  updateProfile,
 } from 'firebase/auth'
 
-export const registerUser = async (email: string, password: string) => {
+type newUser = {
+  email: string
+  password: string
+  studyProgram: string
+  name: string
+}
+
+// registers new user in firebase auth and db
+export const registerUser = async (newUser: newUser) => {
   const auth = getAuth()
-  const credentials = await createUserWithEmailAndPassword(
+  const appStore = useAppStore()
+
+  // make sure onauthStateChanged does not try to fetch
+  // the user data before it is created
+  appStore.registeringUser = true
+
+  // register user in firebase auth
+  const { user } = await createUserWithEmailAndPassword(
     auth,
-    email,
-    password
-  ).catch((error) => {
-    console.error(error.code, error.message)
-    return false
+    newUser.email,
+    newUser.password
+  )
+
+  // set displayname in firebase auth
+  await updateProfile(user, { displayName: newUser.name })
+
+  // get idToken and register user in db
+  const idToken = await user.getIdToken()
+  await $fetch('/api/user', {
+    method: 'POST',
+    headers: {
+      Authorization: `Bearer ${idToken}`,
+    },
+    body: {
+      studyProgram: newUser.studyProgram,
+    },
   })
-  return credentials
+
+  // sign out the new user
+  await signoutUser()
+
+  // navigate to sign in page
+  const localePath = useLocalePath()
+  await navigateTo(localePath('/user/signin'))
+
+  appStore.registeringUser = false
 }
 
 export const signinUser = async (email: string, password: string) => {
@@ -31,13 +67,14 @@ export const signinUser = async (email: string, password: string) => {
   return credentials
 }
 
+// signs out firebase user and removes token cookie
 export const signoutUser = async () => {
   const auth = getAuth()
   const app = useAppStore()
   const authStore = useAuthStore()
-
   const localePath = useLocalePath()
 
+  // close app drawer
   app.drawer = false
 
   // sign out user and navigate to home page
@@ -53,13 +90,20 @@ export const signoutUser = async () => {
     console.log('Could not sign out user')
   }
 
+  // set local auth state to null
   authStore.user = null
 }
 
+// called when the application is started
+// listens for changes in the firebase auth state
 export const initUser = () => {
   const auth = getAuth()
+  const appStore = useAppStore()
 
   onAuthStateChanged(auth, async (user) => {
+    // return if a user is currently being created
+    if (appStore.registeringUser) return
+
     const authStore = useAuthStore()
 
     if (user) {
