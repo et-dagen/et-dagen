036f4bc908fc11f6c400d3008fabe7767c23d440
Ian Philip Eglin <philip@eglin.no>
2023-09-05 08:25:26 +0200

    feat: separate dev and prod firebase projects (#75)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,16 +2,43 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     // used by firebase admin sdk
-    GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    GOOGLE_APPLICATION_CREDENTIALS:
+      process.env.NODE_ENV === 'production'
+        ? process.env.GOOGLE_APPLICATION_CREDENTIALS_PROD
+        : process.env.GOOGLE_APPLICATION_CREDENTIALS_DEV,
     public: {
-      FB_API_KEY: process.env.NUXT_FB_API_KEY,
-      FB_AUTH_DOMAIN: process.env.NUXT_FB_AUTH_DOMAIN,
-      FB_DB_URL: process.env.NUXT_FB_DATABASE_URL,
-      FB_PROJECT_ID: process.env.NUXT_FB_PROJECT_ID,
-      FB_STORAGE_BUCKET: process.env.NUXT_FB_STORAGE_BUCKET,
-      FB_MESSAGING_SENDER_ID: process.env.NUXT_FB_MESSAGING_SENDER_ID,
-      FB_APP_ID: process.env.NUXT_FB_APP_ID,
-      FB_MEASUREMENT_ID: process.env.NUXT_FB_MEASUREMENT_ID,
+      FB_API_KEY:
+        process.env.NODE_ENV === 'production'
+          ? process.env.NUXT_FB_API_KEY_PROD
+          : process.env.NUXT_FB_API_KEY_DEV,
+      FB_AUTH_DOMAIN:
+        process.env.NODE_ENV === 'production'
+          ? process.env.NUXT_FB_AUTH_DOMAIN_PROD
+          : process.env.NUXT_FB_AUTH_DOMAIN_DEV,
+      FB_DB_URL:
+        process.env.NODE_ENV === 'production'
+          ? process.env.NUXT_FB_DATABASE_URL_PROD
+          : process.env.NUXT_FB_DATABASE_URL_DEV,
+      FB_PROJECT_ID:
+        process.env.NODE_ENV === 'production'
+          ? process.env.NUXT_FB_PROJECT_ID_PROD
+          : process.env.NUXT_FB_PROJECT_ID_DEV,
+      FB_STORAGE_BUCKET:
+        process.env.NODE_ENV === 'production'
+          ? process.env.NUXT_FB_STORAGE_BUCKET_PROD
+          : process.env.NUXT_FB_STORAGE_BUCKET_DEV,
+      FB_MESSAGING_SENDER_ID:
+        process.env.NODE_ENV === 'production'
+          ? process.env.NUXT_FB_MESSAGING_SENDER_ID_PROD
+          : process.env.NUXT_FB_MESSAGING_SENDER_ID_DEV,
+      FB_APP_ID:
+        process.env.NODE_ENV === 'production'
+          ? process.env.NUXT_FB_APP_ID_PROD
+          : process.env.NUXT_FB_APP_ID_DEV,
+      FB_MEASUREMENT_ID:
+        process.env.NODE_ENV === 'production'
+          ? process.env.NUXT_FB_MEASUREMENT_ID_PROD
+          : process.env.NUXT_FB_MEASUREMENT_ID_DEV,
     },
   },
 
