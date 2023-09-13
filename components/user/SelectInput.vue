56f00a81c5f2b4cbc01b4dc1340b2b6e21f3daf1
ipeglin <philip@eglin.no>
2023-09-13 20:49:12 +0200

    feat: adding new vuetify select component with props

---
+++ components/user/SelectInput.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  export type SelectOption = string | number
+  export interface SelectInputContent {
+    label: string
+    options: SelectOption[]
+    required?: boolean
+  }
+
+  defineProps({
+    content: { type: Object as PropType<SelectInputContent>, required: true },
+  })
+</script>
+
+<template>
+  <v-select
+    :label="content.label"
+    :items="content.options"
+    variant="outlined"
+  />
+</template>
