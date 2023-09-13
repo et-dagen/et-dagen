5fc6e6389b1dffd8eb44d32f6389210ead20c92e
ipeglin <philip@eglin.no>
2023-09-13 19:51:14 +0200

    feat: adding new vuetify text input component which emits values on @input

---
+++ components/user/TextInput.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  export interface TextInputContent {
+    label: string
+    hint?: string
+    rules?: string[]
+  }
+
+  defineProps({
+    content: { type: Object as PropType<TextInputContent>, required: true },
+  })
+
+  const emit = defineEmits(['input'])
+  const inputValue = ref<string>()
+</script>
+
+<template>
+  <v-text-field
+    :label="content.label"
+    type="text"
+    variant="outlined"
+    :hint="content.hint"
+    v-model="inputValue"
+    @input="() => emit('input', inputValue)"
+  />
+</template>
