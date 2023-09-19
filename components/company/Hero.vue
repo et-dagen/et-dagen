86b8960d237d2466017a4776c8cc758b2ee89716
Joresund <johanstore2002@gmail.com>
2023-09-19 15:22:02 +0200

    refactor: removed excessive styling

--- components/company/Hero.vue
+++ components/company/Hero.vue
@@ -1,26 +1,24 @@
 <template>
-  <div class="my-5 d-flex justify-center">
-    <VSheet id="companyHero">
-      <div class="grid">
-        <v-container>
-          <VImg
-            :src="company.content.logo"
-            :alt="company.content.name + '\'s logo'"
-          />
-        </v-container>
-        <v-container>
-          <h4 v-if="mobile" class="text-h5 font-weight-bold">
-            {{ company.content.name }}
-          </h4>
-          <h4 v-else class="text-h4 font-weight-bold">
-            {{ company.content.name }}
-          </h4>
-          <p>Description: {{ company.content.description }}</p>
-          <h4>Link: <a :href="company.content.website">Vår nettside</a></h4>
-        </v-container>
-      </div>
-    </VSheet>
-  </div>
+  <VSheet id="companyHero">
+    <div class="grid">
+      <v-container>
+        <VImg
+          :src="company.content.logo"
+          :alt="company.content.name + '\'s logo'"
+        />
+      </v-container>
+      <v-container>
+        <h4 v-if="mobile" class="text-h5 font-weight-bold">
+          {{ company.content.name }}
+        </h4>
+        <h4 v-else class="text-h4 font-weight-bold">
+          {{ company.content.name }}
+        </h4>
+        <p>Description: {{ company.content.description }}</p>
+        <h4>Link: <a :href="company.content.website">Vår nettside</a></h4>
+      </v-container>
+    </div>
+  </VSheet>
 </template>
 
 <script setup lang="ts">
