185fcb2b9bc05e4e0e3bafdf11ac42529bb4f6ad
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-09-19 16:39:33 +0200

    temp: endpoint tests

--- pages/index.vue
+++ pages/index.vue
@@ -14,10 +14,48 @@
       "
       >Create user</VBtn
     >
+
+    <VBtn color="success" @click="createCompany">createCompany</VBtn>
+    <VBtn color="success" @click="udpateCompany">udpateCompany</VBtn>
+    <VBtn color="error" @click="deleteCompany">deleteCompany</VBtn>
+
+    {{ companies }}
   </div>
 </template>
 
 <script setup lang="ts">
   const authStore = useAuthStore()
   const { user } = storeToRefs(authStore)
+
+  const { data: companies } = await useFetch(`/api/company`)
+
+  const createCompany = async () => {
+    await $fetch('/api/company', {
+      method: 'POST',
+      body: {
+        name: 'Company of Things',
+        webpage: 'cot.no',
+        type: 'partner',
+      },
+    })
+  }
+
+  const udpateCompany = async () => {
+    await $fetch('/api/company', {
+      method: 'PUT',
+      body: {
+        name: 'Company of Things',
+        companyUID: '-NehfYyZOHF6-6aYVcx8',
+      },
+    })
+  }
+
+  const deleteCompany = async () => {
+    await $fetch('/api/company', {
+      method: 'DELETE',
+      body: {
+        companyUID: '-NehfYyZOHF6-6aYVcx8',
+      },
+    })
+  }
 </script>
