7a8fd2d66839051429bf739d00ecd0193c1fc7b4
Joresund <johanstore2002@gmail.com>
2023-09-19 17:26:29 +0200

    fix: made the div centered on frontpage

--- components/company/Hero.vue
+++ components/company/Hero.vue
@@ -1,24 +1,38 @@
 <template>
-  <VSheet id="companyHero">
-    <div class="grid">
-      <v-container>
-        <VImg
-          :src="company.content.logo"
-          :alt="company.content.name + '\'s logo'"
-        />
-      </v-container>
-      <v-container>
-        <h4 v-if="mobile" class="text-h5 font-weight-bold">
-          {{ company.content.name }}
-        </h4>
-        <h4 v-else class="text-h4 font-weight-bold">
-          {{ company.content.name }}
-        </h4>
-        <p>Description: {{ company.content.description }}</p>
-        <h4>Link: <a :href="company.content.website">Vår nettside</a></h4>
-      </v-container>
-    </div>
-  </VSheet>
+  <div>
+    <VSheet id="companyHero">
+      <div class="grid">
+        <v-container>
+          <VImg
+            :src="company.content.logo"
+            :alt="company.content.name + '\'s logo'"
+          />
+        </v-container>
+        <v-container>
+          <h4 v-if="mobile" class="text-h5 font-weight-bold">
+            {{ company.content.name }}
+          </h4>
+          <h4 v-else class="text-h4 font-weight-bold">
+            {{ company.content.name }}
+          </h4>
+          <p>Description: {{ company.content.description }}</p>
+          <p>
+            Description: Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Amet cursus sit amet dictum sit amet justo donec enim.
+            Condimentum vitae sapien pellentesque habitant morbi tristique
+            senectus et. Faucibus a pellentesque sit amet porttitor eget dolor
+            morbi. Lacus vestibulum sed arcu non odio euismod lacinia at. Auctor
+            neque vitae tempus quam pellentesque nec nam aliquam. Leo a diam
+            sollicitudin tempor. Cras tincidunt lobortis feugiat vivamus at
+            augue eget. Ullamcorper malesuada proin libero nunc. Ornare massa
+            eget egestas purus viverra accumsan in.
+          </p>
+          <h4>Link: <a :href="company.content.website">Vår nettside</a></h4>
+        </v-container>
+      </div>
+    </VSheet>
+  </div>
 </template>
 
 <script setup lang="ts">
