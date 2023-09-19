04c219ff9c7888dd46d15cf1d404f06740ccab19
Reidar Arne Eidsvik Nerheim <raener12@gmail.com>
2023-09-19 14:23:41 +0200

    Test

--- app.vue
+++ app.vue
@@ -4,6 +4,7 @@
       <!-- page transition -->
       <VFadeTransition mode="out-in">
         <div :key="$route.fullPath">
+          <!--Test-->
           <NuxtPage />
         </div>
       </VFadeTransition>
