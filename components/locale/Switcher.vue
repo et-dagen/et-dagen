189d8c67b1ce5885ad67ecde02227031f73f0044
Reidar Arne Eidsvik Nerheim <raener12@gmail.com>
2023-09-19 16:22:03 +0200

    Fix: added lang="ts", and cast the locales

--- components/locale/Switcher.vue
+++ components/locale/Switcher.vue
@@ -1,13 +1,20 @@
-<script setup>
+<script setup lang="ts">
+  import { LocaleObject } from '@nuxtjs/i18n/dist/runtime/composables'
+
   const { locale, locales } = useI18n()
   // generates URLs to switch between locales
   const switchLocalePath = useSwitchLocalePath()
+
   const availableLocales = computed(() => {
     // returns a list of locales, exluding current locale
-    return locales.value.filter((i) => i.code !== locale.value)
+    return (locales.value as LocaleObject[]).filter(
+      (language) => language.code !== locale.value
+    )
   })
   const currentLocale = computed(() => {
-    return locales.value.find((i) => i.code === locale.value)
+    return (locales.value as LocaleObject[]).find(
+      (language) => language.code === locale.value
+    )
   })
 </script>
 
@@ -18,24 +25,24 @@
       <VBtn v-bind="props" rounded="lg" variant="text" class="mx-2">
         <!-- dependent on current locale-->
         <template #prepend>
-          <VImg width="30" :src="`/images/flags/${currentLocale.code}.svg`" />
+          <VImg width="30" :src="`/images/flags/${currentLocale?.code}.svg`" />
         </template>
-        {{ currentLocale.name.toUpperCase() }}
+        {{ currentLocale?.name?.toUpperCase() }}
       </VBtn>
     </template>
 
     <VCard class="d-flex flex-column mt-2 pa-2" rounded="lg" :elevation="3">
       <!-- buttons for locales -->
       <VBtn
-        variant="text"
         v-for="language in availableLocales"
         :key="language.code"
+        variant="text"
         :to="switchLocalePath(language.code)"
       >
         <template #prepend>
           <VImg width="30" :src="`/images/flags/${language.code}.svg`" />
         </template>
-        {{ language.name.toUpperCase() }}
+        {{ language.name?.toUpperCase() }}
       </VBtn>
     </VCard>
   </VMenu>
