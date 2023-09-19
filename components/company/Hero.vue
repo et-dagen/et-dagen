c0e51913d8a28f0864cbac0a9a83539c299333c0
Joresund <johanstore2002@gmail.com>
2023-09-19 15:16:48 +0200

    fix: problems with auto-scaling

--- components/company/Hero.vue
+++ components/company/Hero.vue
@@ -1,61 +1,22 @@
 <template>
   <div class="my-5 d-flex justify-center">
-    <VSheet
-      style="
-        border-width: 2px;
-        border-color: black;
-        overflow: hidden;
-        max-height: 500px;
-        max-width: 70vw;
-      "
-    >
+    <VSheet id="companyHero">
       <div class="grid">
-        <v-container class="text-right pa-0 w-100">
-          <!-- <p>{{ company.content.logo }}</p> -->
+        <v-container>
           <VImg
-            src="https://media.istockphoto.com/id/178447404/photo/
-            modern-business-buildings.jpg?s=612x612&w=0&k=20&c=MOG
-            9lvRz7WjsVyW3IiQ0srEzpaBPDcc7qxYsBCvAUJs="
-            alt=""
+            :src="company.content.logo"
+            :alt="company.content.name + '\'s logo'"
           />
         </v-container>
-        <v-container class="pa-1" style="overflow-wrap: break-word">
-          <h4 v-if="mobile" class="text-h6 font-weight-bold">
+        <v-container>
+          <h4 v-if="mobile" class="text-h5 font-weight-bold">
             {{ company.content.name }}
           </h4>
-          <h4 v-else class="text-h3">
+          <h4 v-else class="text-h4 font-weight-bold">
             {{ company.content.name }}
           </h4>
-          <!-- <p>Description: {{ company.content.description }}</p> -->
-          <p>
-            Description: Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Amet cursus sit amet dictum sit amet justo donec enim.
-            Condimentum vitae sapien pellentesque habitant morbi tristique
-            senectus et. Faucibus a pellentesque sit amet porttitor eget dolor
-            morbi. Lacus vestibulum sed arcu non odio euismod lacinia at. Auctor
-            neque vitae tempus quam pellentesque nec nam aliquam. Leo a diam
-            sollicitudin tempor. Cras tincidunt lobortis feugiat vivamus at
-            augue eget. Ullamcorper malesuada proin libero nunc. Ornare massa
-            eget egestas purus viverra accumsan in. Ultricies lacus sed turpis
-            tincidunt. Orci dapibus ultrices in iaculis nunc sed. Urna nec
-            tincidunt praesent semper feugiat nibh sed pulvinar. Pharetra massa
-            massa ultricies mi quis hendrerit dolor magna eget. Donec ultrices
-            tincidunt arcu non sodales neque sodales ut etiam. Faucibus interdum
-            posuere lorem ipsum dolor. Nunc congue nisi vitae suscipit tellus.
-            Id neque aliquam vestibulum morbi blandit. Diam phasellus vestibulum
-            lorem sed risus ultricies tristique nulla.
-          </p>
+          <p>Description: {{ company.content.description }}</p>
           <h4>Link: <a :href="company.content.website">Vår nettside</a></h4>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
         </v-container>
       </div>
     </VSheet>
@@ -71,13 +32,20 @@
       required: true,
     },
   })
-
-  console.log(`Company: ${JSON.stringify(company)}`)
 </script>
 
 <style scoped>
+  #companyHero {
+    max-width: 70vw;
+  }
+
+  @media screen and (max-width: 490px) {
+    #companyHero {
+      max-width: 90vw;
+    }
+  }
   .grid {
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(210px, 1fr));
   }
 </style>
