9d26ce73babf98a72492d019c927751ae6105928
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-09-21 22:43:41 +0200

    chore: remove tests from index page

--- pages/index.vue
+++ pages/index.vue
@@ -8,45 +8,5 @@
         title: 'Elektronikk & Teknologidagen',
       }"
     />
-
-    <VBtn color="success" @click="createCompany">createCompany</VBtn>
-    <VBtn color="success" @click="udpateCompany">udpateCompany</VBtn>
-    <VBtn color="error" @click="deleteCompany">deleteCompany</VBtn>
-
-    {{ companies }}
   </div>
 </template>
-
-<script setup lang="ts">
-  const { data: companies } = await useFetch(`/api/company`)
-
-  const createCompany = async () => {
-    await $fetch('/api/company', {
-      method: 'POST',
-      body: {
-        name: 'Company of Things',
-        webpage: 'cot.no',
-        type: 'partner',
-      },
-    })
-  }
-
-  const udpateCompany = async () => {
-    await $fetch('/api/company', {
-      method: 'PUT',
-      body: {
-        name: 'Company of Things',
-        companyUID: '-NehfYyZOHF6-6aYVcx8',
-      },
-    })
-  }
-
-  const deleteCompany = async () => {
-    await $fetch('/api/company', {
-      method: 'DELETE',
-      body: {
-        companyUID: '-NehfYyZOHF6-6aYVcx8',
-      },
-    })
-  }
-</script>
