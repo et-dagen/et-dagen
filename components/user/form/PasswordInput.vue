138b108d2d3012bb7578a0dd60c1a200ba41bdde
Siri Stølevik <siri.stolevik@gmail.com>
2023-09-21 09:31:43 +0200

    fix: switched places for template and script and imported password validation composable

--- components/user/form/PasswordInput.vue
+++ components/user/form/PasswordInput.vue
@@ -1,9 +1,17 @@
+<script setup lang="ts">
+  import { useRequiredInput, useValidatePassword } from '@/composables/useForm'
+  defineProps({
+    confirmPassword: Boolean,
+  })
+  const ShowPassord = ref(false)
+</script>
+
 <template>
   <VResponsive max-width="400">
     <VTextField
       variant="outlined"
       clearable
-      :rules="[rules.required, rules.min]"
+      :rules="[useRequiredInput, useValidatePassword]"
       :append-inner-icon="ShowPassord ? 'mdi-eye' : 'mdi-eye-off'"
       :type="ShowPassord ? 'text' : 'password'"
       :label="confirmPassword ? 'Bekreft passord' : 'Passord'"
@@ -11,15 +19,3 @@
     />
   </VResponsive>
 </template>
-
-<script setup lang="ts">
-  defineProps({
-    confirmPassword: Boolean,
-  })
-  const ShowPassord = ref(false)
-
-  const rules = {
-    required: (value: string) => !!value || 'Feltet er påkrevd',
-    min: (value: string) => value.length >= 8 || 'Passordet må ha minst 8 tegn',
-  }
-</script>
