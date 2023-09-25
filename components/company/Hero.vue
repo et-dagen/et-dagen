2ee95d6949f0dafa76e0dbe510d5ffd00771cbb1
Joresund <johanstore2002@gmail.com>
2023-09-25 12:41:41 +0200

    refactor: minor fixes

--- components/company/Hero.vue
+++ components/company/Hero.vue
@@ -1,34 +1,3 @@
-<template>
-  <div>
-    <v-sheet class="w-75">
-      <v-row no-gutters>
-        <v-col cols="12" lg="6" style="cursor: pointer" @click="redirect()">
-          <v-img
-            class="w-100"
-            :src="company.content.logo"
-            :alt="`${company.content.name}'s logo`"
-          />
-        </v-col>
-        <v-col
-          cols="12"
-          lg="6"
-          style="max-height: 40vh"
-          class="overflow-y-auto"
-        >
-          <h4
-            class="text-lg-h4 text-h5 font-weight-bold w-auto d-inline-block"
-            style="cursor: pointer"
-            @click="redirect()"
-          >
-            {{ company.content.name }} &#8599;
-          </h4>
-          <p>{{ company.content.description }}</p>
-        </v-col>
-      </v-row>
-    </v-sheet>
-  </div>
-</template>
-
 <script setup lang="ts">
   interface CompanyInfo {
     name: string
@@ -43,8 +12,35 @@
       required: true,
     },
   })
-
-  const redirect = () => {
-    window.location.replace(company.content.website)
-  }
 </script>
+
+<template>
+  <div>
+    <VSheet class="w-75">
+      <VRow no-gutters>
+        <VCol
+          cols="12"
+          lg="6"
+          style="cursor: pointer"
+          @click="navigateTo(company.content.website, { external: true })"
+        >
+          <VImg
+            class="w-100"
+            :src="company.content.logo"
+            :alt="`${company.content.name}'s logo`"
+          />
+        </VCol>
+        <VCol cols="12" lg="6" style="max-height: 40vh" class="overflow-y-auto">
+          <h4
+            class="text-lg-h4 text-h5 font-weight-bold w-auto d-inline-block"
+            style="cursor: pointer"
+            @click="navigateTo(company.content.website, { external: true })"
+          >
+            {{ company.content.name }} &#8599;
+          </h4>
+          <p>{{ company.content.description }}</p>
+        </VCol>
+      </VRow>
+    </VSheet>
+  </div>
+</template>
