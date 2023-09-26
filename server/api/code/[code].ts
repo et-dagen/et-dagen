e42e0c68bba203e15f02b3098c456460c4193746
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-09-26 17:53:32 +0200

    docs: validate code endpoint comments

--- server/api/code/[code].ts
+++ server/api/code/[code].ts
@@ -4,8 +4,10 @@
 export default defineEventHandler(async (event) => {
   const code = getRouterParam(event, 'code') as string
 
+  // reference to registration codes in db
   const registrationCodes = db.ref('registrationCodes')
 
+  // fetch code from db
   const snapshot = await registrationCodes
     .orderByChild('value')
     .equalTo(code)
