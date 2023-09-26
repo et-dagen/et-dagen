e4492e2bd21e15b48c4e146def6f79546321d923
ipeglin <philip@eglin.no>
2023-09-26 16:26:44 +0200

    refactor: moving script tag to top of file, and changing tags to PascalCase

--- components/company/Card.vue
+++ components/company/Card.vue
@@ -1,19 +1,3 @@
-<template>
-  <v-hover v-slot="{ isHovering, props }">
-    <v-card
-      :elevation="isHovering ? 10 : 0"
-      :class="{ 'on-hover': isHovering }"
-      class="mx-auto d-flex align-center justify-center"
-      height="125"
-      max-width="250"
-      v-bind="props"
-      @click="navigateTo(content.website, { external: true, open: 'target' })"
-    >
-      <v-img :width="500" aspect-ratio="4/3" cover :src="content.logo" />
-    </v-card>
-  </v-hover>
-</template>
-
 <script setup lang="ts">
   defineProps<{
     content: {
@@ -22,3 +6,19 @@
     }
   }>()
 </script>
+
+<template>
+  <VHover v-slot="{ isHovering, props }">
+    <VCard
+      :elevation="isHovering ? 10 : 0"
+      :class="{ 'on-hover': isHovering }"
+      class="mx-auto d-flex align-center justify-center"
+      height="125"
+      max-width="250"
+      v-bind="props"
+      @click="navigateTo(content.website, { external: true, open: 'target' })"
+    >
+      <VImg :width="500" aspect-ratio="4/3" cover :src="content.logo" />
+    </VCard>
+  </VHover>
+</template>
