00d561f250c7afb2ceeb3c7115487f9a6c502803
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-09-26 16:45:26 +0200

    feat: delete registration code endpoint

---
+++ server/api/code/index.delete.ts
@@ -0,0 +1,22 @@
+// DELETE /api/company
+// endpoint for removing a registration code from db
+
+export default defineEventHandler(async (event) => {
+  const { user } = event.context
+
+  // only admins can remove registration codes
+  if (!hasAccess(user, ['admin']))
+    throw createError({
+      statusCode: 401,
+      statusMessage: 'User not authorized',
+    })
+
+  // get request body
+  const { code } = await readBody(event)
+
+  // delete code
+  deleteCode(code)
+
+  // company successfully removed
+  sendNoContent(event, 204)
+})
