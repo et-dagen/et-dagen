db875f7230393e84ef5df0b4cfbd08650012dc68
Siri Stølevik <siri.stolevik@gmail.com>
2023-10-03 16:35:04 +0200

    feat: add text info

---
+++ components/contact/Card.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  defineProps<{
+    content: {
+      name: string
+      image: string
+      position: string
+      email: string
+      phone: string
+    }
+  }>()
+</script>
+
+<template>
+  <div class="d-flex align-center flex-column">
+    <VCard width="500">
+      <VCardTitle class="text-h4">
+        {{ content.name }}
+      </VCardTitle>
+      <VCardText class="text-h6"> <br />{{ content.position }} </VCardText>
+      <VCardText class="text-body-1">
+        {{ content.email }}
+      </VCardText>
+      <VCardText class="text-body-1">
+        {{ content.phone }}
+      </VCardText>
+    </VCard>
+  </div>
+</template>
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,3 +1,12 @@
 <template>
-  <div>Contact page</div>
+  <div>
+    <ContactCard
+      :content="{
+        name: 'Ian Philip Eglin',
+        position: 'IT-ansvarlig',
+        email: 'itansvarlig@et-dagen.no',
+        phone: '+47 97085256',
+      }"
+    />
+  </div>
 </template>
