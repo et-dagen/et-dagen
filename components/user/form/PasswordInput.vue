8540cf93d38cf794d45a4a49fa8f3f396337e5b4
ipeglin <philip@eglin.no>
2023-10-03 20:20:52 +0200

    feat: merging in EmailInput and PasswordInput components from other branches

--- components/user/form/PasswordInput.vue
+++ components/user/form/PasswordInput.vue
@@ -1,25 +1,23 @@
 <script setup lang="ts">
-  import { useRequiredInput, useValidatePassword } from '@/composables/useForm'
   defineProps({
     confirmPassword: Boolean,
+    clearable: Boolean,
   })
+
   const ShowPassord = ref(false)
 </script>
 
 <template>
-  <VResponsive max-width="400">
-    <VTextField
-      variant="outlined"
-      clearable
-      :rules="[useRequiredInput, useValidatePassword]"
-      :append-inner-icon="ShowPassord ? 'mdi-eye' : 'mdi-eye-off'"
-      :type="ShowPassord ? 'text' : 'password'"
-      :label="
-        confirmPassword
-          ? $t('user.register.password_confirmation')
-          : $t('user.register.password')
-      "
-      @click:append-inner="ShowPassord = !ShowPassord"
-    />
-  </VResponsive>
+  <VTextField
+    variant="outlined"
+    :clearable="clearable"
+    :append-inner-icon="ShowPassord ? 'mdi-eye' : 'mdi-eye-off'"
+    :type="ShowPassord ? 'text' : 'password'"
+    :label="
+      confirmPassword
+        ? $t('user.register.password_confirmation')
+        : $t('user.register.password')
+    "
+    @click:append-inner="ShowPassord = !ShowPassord"
+  />
 </template>
