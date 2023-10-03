8ce965406646a168882a0f14c52fd98439b67b8a
Miriam <miriam.matsson@gmail.com>
2023-10-03 15:19:07 +0200

    feat: adding email input

--- components/user/EmailInput.vue
+++ components/user/EmailInput.vue
@@ -1,28 +1,20 @@
 <template>
-  <div>
-    <v-text-field
-      placeholder="ola@nordmann.no"
-      type="email"
-      :rules="[rules.required, rules.email]"
-      :v-bind="emailInput"
-      variant="outlined"
-      @update:model-value="emailInput"
-    />
-  </div>
+  <VTextField
+    placeholder="olakari@nordmann.no"
+    type="email"
+    :rules="[rules.email]"
+    variant="outlined"
+  />
 </template>
 
 <script setup lang="ts">
-  const { t } = useI18n()
-
-  let emailInput: string
   const rules = {
-    required: (value: string) => !!value || t('user.required_input'),
     email: (value: string) => {
       // prettier-ignore-start
       const pattern =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       // prettier-ignore-end
-      return pattern.test(value) || t('user.invalid_email')
+      return pattern.test(value) || 'user.invalid_email'
     },
   }
 </script>
