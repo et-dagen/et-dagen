1168401b020c4b790e9eba2f9467945c71d55aaa
Siri Stølevik <siri.stolevik@gmail.com>
2023-10-04 09:24:53 +0200

    feat: add english and norwegian version of positions

--- components/contact/Card.vue
+++ components/contact/Card.vue
@@ -11,16 +11,14 @@
 </script>
 
 <template>
-  <div class="d-flex align-center flex-column">
+  <div class="d-flex flex-column">
     <VCard width="500">
       <VCardTitle class="text-h4">
         {{ content.name }}
       </VCardTitle>
       <VCardText class="text-h6"> <br />{{ content.position }} </VCardText>
       <VCardText class="text-body-1">
-        {{ content.email }}
-      </VCardText>
-      <VCardText class="text-body-1">
+        {{ content.email }} <br /><br />
         {{ content.phone }}
       </VCardText>
     </VCard>
--- pages/contact.vue
+++ pages/contact.vue
@@ -3,9 +3,9 @@
     <ContactCard
       :content="{
         name: 'Ian Philip Eglin',
-        position: 'IT-ansvarlig',
+        position: $t('contact.positions.IT'),
         email: 'itansvarlig@et-dagen.no',
-        phone: '+47 97085256',
+        phone: '+47 123 45 678',
       }"
     />
   </div>
