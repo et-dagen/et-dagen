c686967a6a96dfa259f802a2578e547cede05880
Siri Stølevik <siri.stolevik@gmail.com>
2023-10-04 10:49:03 +0200

    feat: add picture

--- components/contact/Card.vue
+++ components/contact/Card.vue
@@ -12,15 +12,28 @@
 
 <template>
   <div class="d-flex flex-column">
-    <VCard width="500">
-      <VCardTitle class="text-h4">
-        {{ content.name }}
-      </VCardTitle>
-      <VCardText class="text-h6"> <br />{{ content.position }} </VCardText>
-      <VCardText class="text-body-1">
-        {{ content.email }} <br /><br />
-        {{ content.phone }}
-      </VCardText>
+    <VCard class="mx-auto" width="435" variant="flat">
+      <VRow>
+        <VCol>
+          <VImg
+            height="100%"
+            class="rounded-lg"
+            :width="150"
+            :src="content.image"
+            cover
+          />
+        </VCol>
+        <VCol>
+          <VCardTitle class="text-h4">
+            {{ content.name }}
+          </VCardTitle>
+          <VCardText class="text-h6"> <br />{{ content.position }} </VCardText>
+          <VCardText class="text-body-1">
+            {{ content.email }} <br /><br />
+            {{ content.phone }}
+          </VCardText>
+        </VCol>
+      </VRow>
     </VCard>
   </div>
 </template>
--- pages/contact.vue
+++ pages/contact.vue
@@ -6,6 +6,7 @@
         position: $t('contact.positions.IT'),
         email: 'itansvarlig@et-dagen.no',
         phone: '+47 123 45 678',
+        image: 'https://picsum.photos/500/300?image=60',
       }"
     />
   </div>
