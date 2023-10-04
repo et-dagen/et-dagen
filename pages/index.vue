73a02a784305d815468c52b0207b04d9851baf96
Aasmund Nørsett <aasmund.norsett@gmail.com>
2023-10-04 22:50:28 +0200

    chore: remove tests

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,3 @@
-<script setup lang="ts">
-  const createCode = async () => {
-    await $fetch('/api/code', {
-      method: 'POST',
-      query: {
-        companyUID: '-NesGFDmavw-y88kvfYM',
-      },
-    })
-  }
-
-  const code = 'company-of-things-utcUHFsp'
-
-  const checkCode = async () => {
-    await $fetch(`/api/code/${code}`)
-  }
-
-  const deleteCode = async () => {
-    await $fetch(`/api/code`, {
-      method: 'DELETE',
-      body: {
-        code,
-      },
-    })
-  }
-</script>
-
 <template>
   <div>
     <HomeBanner
@@ -34,9 +8,5 @@
         title: 'Elektronikk & Teknologidagen',
       }"
     />
-
-    <VBtn color="success" @click="createCode">Create Code</VBtn>
-    <VBtn color="success" @click="checkCode">Check Code</VBtn>
-    <VBtn color="error" @click="deleteCode">Delete Code</VBtn>
   </div>
 </template>
