0ce12b88bf164d924bc60901525dd0e0754f8f2d
Siri Stølevik <siri.stolevik@gmail.com>
2023-10-06 14:39:06 +0200

    feat:add mailto: and tel:

--- components/contact/Card.vue
+++ components/contact/Card.vue
@@ -29,8 +29,10 @@
           </VCardTitle>
           <VCardText class="text-h6"> <br />{{ content.position }} </VCardText>
           <VCardText class="text-body-1">
-            {{ content.email }} <br /><br />
-            {{ content.phone }}
+            <a :href="`mailto:${content.email}`"> {{ content.email }} </a>
+          </VCardText>
+          <VCardText class="text-body-1">
+            <a :href="`tel:${content.phone}`"> {{ content.phone }} </a>
           </VCardText>
         </VCol>
       </VRow>
