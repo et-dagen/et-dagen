68303a77da93cd9ae7811a1188f3cc505128e03b
Joresund <johanstore2002@gmail.com>
2023-10-10 16:06:24 +0200

    feat: onhover transition and image styling

--- components/company/Hero.vue
+++ components/company/Hero.vue
@@ -6,14 +6,7 @@
     website: string
   }
 
-  const company = defineProps({
-    content: {
-      type: Object as PropType<CompanyInfo>,
-      required: true,
-    },
-  })
-
-  const openNewTab = () => {
+  const openInNewTab = () => {
     navigateTo(company.content.website, {
       external: true,
       open: {
@@ -21,54 +14,77 @@
       },
     })
   }
+
+  const company = defineProps({
+    content: {
+      type: Object as PropType<CompanyInfo>,
+      required: true,
+    },
+  })
+  const { lgAndUp } = useDisplay()
 </script>
 
 <template>
   <div>
-    <VSheet class="w-75">
+    <VSheet :width="lgAndUp ? '75vw' : '90vw'">
+      <br />
       <h2
-        class="text-md-h2 text-sm-h3 text-h4 text-center pb-10 font-weight-bold"
+        :class="`text-md-h2 text-sm-h3 text-h4 text-center 
+        pt-10 pb-lg-3 pb-5 font-weight-bold`"
       >
         {{ $t('company.main_partner') }}
       </h2>
 
-      <VRow class="pt-10">
+      <VRow class="pt-lg-10" no-gutters>
         <!-- Company logo -->
-        <VCol cols="12" lg="6" style="cursor: pointer">
-          <VHover v-slot="{ isHovering, props }">
-            <VCard
-              :elevation="isHovering ? 20 : 0"
-              :class="{ 'on-hover': isHovering }"
-              v-bind="props"
-              @click="openNewTab()"
-            >
-              <VImg
-                class="w-100 elevation-3 rounded-lg"
-                :src="company.content.logo"
-                :alt="`${company.content.name}'s logo`"
-              />
-            </VCard>
-          </VHover>
+        <VCol cols="12" lg="6" style="cursor: pointer" class="pr-lg-3">
+          <VImg
+            class="w-100 rounded-lg companyImage"
+            :src="company.content.logo"
+            :alt="`${company.content.name}'s logo`"
+            @click="openInNewTab"
+          />
         </VCol>
 
         <!-- Company name and description -->
-        <!-- Scroll bar if text is too long -->
-        <VCol cols="12" lg="6" style="max-height: 40vh" class="overflow-y-auto">
+        <VCol
+          cols="12"
+          lg="6"
+          style="max-height: 40vh"
+          class="pt-lg-0 pt-3 pl-lg-3"
+        >
           <VHover v-slot="{ isHovering, props }">
-            <VCard
-              :elevation="isHovering ? 20 : 0"
+            <VSheet
               :class="{ 'on-hover': isHovering }"
               v-bind="props"
-              class="text-lg-h4 text-h5 font-weight-bold d-inline-block"
-              style="cursor: pointer"
-              @click="openNewTab()"
+              class="d-inline-block"
+              :style="isHovering ? 'cursor: pointer' : ''"
+              @click="openInNewTab"
             >
-              {{ company.content.name }} &#8599;
-            </VCard>
+              <h3
+                :class="isHovering ? 'text-decoration-underline' : ''"
+                class="text-lg-h4 text-h5 font-weight-bold"
+              >
+                {{ company.content.name }}
+              </h3>
+            </VSheet>
           </VHover>
-          <p>{{ company.content.description }}</p>
+          <p class="pt-1 pt-lg-3">{{ company.content.description }}</p>
         </VCol>
       </VRow>
     </VSheet>
   </div>
 </template>
+
+<style setup lang="scss">
+  .companyImage {
+    color: transparent;
+    background-color: transparent;
+    transition: all 0.2s cubic-bezier(0.25, 0.46, 0.45, 0.94);
+    filter: drop-shadow(2px 2px 1px gray);
+    &:hover {
+      filter: drop-shadow(4px 4px 2px gray);
+      transform: scale(1.05);
+    }
+  }
+</style>
