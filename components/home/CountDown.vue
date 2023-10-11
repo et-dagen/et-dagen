374492c9c8489edd7b286960f782f8f6479b1d69
Miriam <miriam.matsson@gmail.com>
2023-10-11 20:07:42 +0200

    feature: added styling

--- components/home/CountDown.vue
+++ components/home/CountDown.vue
@@ -20,12 +20,74 @@
     cdProps.content.futureTime
   )
 
-  const diffDays = timeDiff.days
-  const diffHours = timeDiff.hours
-  const diffMinutes = timeDiff.minutes
-  const diffSeconds = timeDiff.seconds
+  const displayText = {
+    days: 'Dager',
+    hours: 'Timer',
+    minutes: 'Minutter',
+    seconds: 'Sekunder',
+  }
 </script>
 
 <template>
-  <div>{{ diffDays }}{{ diffHours }}{{ diffMinutes }}{{ diffSeconds }}</div>
+  <v-card width="37%" class="mx-auto" elevation="0">
+    <v-row justify="center">
+      <v-col class="text-center">
+        <v-card-title class="text-h6 text-md-h5 text-lg-h4"
+          >Arrangementet tar av om</v-card-title
+        >
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col
+        v-for="elements in timeDiff"
+        :key="elements"
+        cols="12"
+        md="3"
+        sm="3"
+        class="mx-auto"
+      >
+        <v-card class="outlined">
+          <v-card-text class="text-center">
+            <h4>{{ elements }}</h4>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col
+        v-for="words in displayText"
+        :key="words"
+        cols="12"
+        md="3"
+        sm="3"
+        class="mx-auto"
+      >
+        <v-card-text class="text-center">
+          <h6>{{ words }}</h6>
+        </v-card-text>
+      </v-col>
+    </v-row>
+    <v-card-actions>
+      <v-btn
+        variant="outlined"
+        width="95%"
+        class="mx-auto"
+        style="background-color: #c33c3b"
+        color="white"
+        append-icon="mdi-arrow-right"
+      >
+        SE PROGRAM
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
+
+<style scoped>
+  .text-center {
+    text-align: center;
+  }
+
+  .mx-auto {
+    margin: 0 auto;
+  }
+</style>
