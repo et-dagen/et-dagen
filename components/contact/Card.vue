73997f1e400877b7a464e3fd07c0c5b68a360250
Siri Stølevik <siri.stolevik@gmail.com>
2023-10-13 14:01:40 +0200

    feat: add icons for email and phone

--- components/contact/Card.vue
+++ components/contact/Card.vue
@@ -13,8 +13,8 @@
 <template>
   <VCard variant="flat">
     <VRow>
-      <VCol cols="4">
-        <VImg class="rounded-lg" :src="content.image" />
+      <VCol cols="3">
+        <VImg class="rounded-lg" :src="content.image" height="100%" cover />
       </VCol>
       <VCol>
         <VCardTitle class="font-weight-bold text-h4">
@@ -22,12 +22,37 @@
         </VCardTitle>
         <VCardText class="text-h6"> <br />{{ content.position }} </VCardText>
         <VCardText class="text-body-1">
-          <a :href="`mailto:${content.email}`"> {{ content.email }} </a>
+          <VRow>
+            <VCol cols="1">
+              <div class="icon">
+                <VIcon left>mdi-email-outline </VIcon>
+              </div>
+            </VCol>
+            <VCol>
+              <a :href="`mailto:${content.email}`"> {{ content.email }} </a>
+            </VCol>
+          </VRow>
         </VCardText>
         <VCardText class="text-body-1">
-          <a :href="`tel:${content.phone}`"> {{ content.phone }} </a>
+          <VRow>
+            <VCol cols="1">
+              <div class="icon">
+                <VIcon left>mdi-phone </VIcon>
+              </div>
+            </VCol>
+            <VCol>
+              <a :href="`tel:${content.phone}`"> {{ content.phone }} </a>
+            </VCol>
+          </VRow>
         </VCardText>
       </VCol>
     </VRow>
   </VCard>
 </template>
+
+<style setup lang="scss">
+  @use 'vuetify/settings';
+  .icon {
+    color: rgb(var(--v-theme-primary));
+  }
+</style>
