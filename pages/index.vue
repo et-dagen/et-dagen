4cb0734dea4eda5615788ac767bcb849616f05a1
Joresund <johanstore2002@gmail.com>
2023-10-20 17:02:02 +0200

    refactor: added plural form to homebanner component

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         caption: 'Få et innblikk i din fremtidige arbeidsplass',
         date: { start: '2023-02-14', end: '2023-02-15' },
         image: 'https://cdn.vuetifyjs.com/images/parallax/material.jpg',
-        title: 'Elektronikk & Teknologidagen',
+        title: 'Elektronikk & Teknologidagene',
       }"
     />
     <CompanyHero
