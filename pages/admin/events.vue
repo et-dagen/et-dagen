878b3067243583bf64822ab941d8b3c356d67b48
ipeglin <philip@eglin.no>
2023-11-08 00:19:45 +0100

    refactor: changing the presentation of events into a vueitfy table

--- pages/admin/events.vue
+++ pages/admin/events.vue
@@ -1,164 +1,348 @@
 <script setup lang="ts">
-  const { data } = await useFetch('/api/event')
+  import type { Event } from '~/models/Event'
+
+  // Get events and companies from the API
+  const { data: events, refresh } = await useFetch('/api/event')
   const { data: companies } = await useFetch('/api/company')
 
-  // Add event id as property
-  const events = computed(() => {
-    return addEventIDAsProperty(data.value)
+  // Allow copying the event id to the clipboard
+  const { text, copy, copied } = useClipboard()
+
+  // Proces events and retrieve dates
+  const eventsWithID = computed(() => addEventIDAsProperty(events.value))
+  const eventDates = computed(() => getEventDates(events.value))
+
+  // State for filtering and selecting events
+  const selectedDates = ref<number[]>(eventDates.value.map((_, i) => i))
+  const filterOrder = ref('descending')
+  const filterType = ref('updated')
+
+  const selected = ref([])
+
+  // Pagination state
+  const currentPage = ref(1)
+  const pageSizes = [10, 25, 100]
+  const pageSize = ref(pageSizes[0])
+
+  // State for the delete modal
+  const loading = ref()
+  const dialog = ref(false)
+
+  // Reset selected events when changing the page or page size
+  watch(currentPage, () => (selected.value = []))
+  watch(pageSize, () => {
+    currentPage.value = 1
   })
 
-  // Check if event has attendants
-  const hasAttendants = computed(() => {
-    return (event: any) => {
-      return Object.hasOwn(event, 'attendants')
+  // Filter events by date and sort them
+  const filteredEvents = computed(() => {
+    // eslint-disable-next-line
+    const selectedEventDate = eventDates.value.filter((date, index) =>
+      selectedDates.value.includes(index)
+    )
+
+    const filteredByEventDate = Object.values(eventsWithID.value)?.filter(
+      (event: any) =>
+        selectedEventDate.includes(getDateFromDatetime(event.date.start))
+    )
+
+    selected.value = []
+
+    return (
+      filteredByEventDate?.sort((a, b) => {
+        /* @ts-ignore */
+        const order = a[filterType.value] < b[filterType.value] ? -1 : 1
+        return filterOrder.value === 'ascending' ? order : order * -1
+      }) ?? []
+    )
+  })
+
+  // Split events into pages
+  const pages = computed(() =>
+    filteredEvents.value.reduce((resultArray, item, index) => {
+      const chunkIndex = Math.floor(index / pageSize.value)
+      if (!resultArray[chunkIndex]) {
+        resultArray[chunkIndex] = []
+      }
+      resultArray[chunkIndex].push(item)
+      return resultArray
+    }, [] as Event[][])
+  )
+  // Delete seleceted events
+  const deleteEvents = async () => {
+    loading.value = true
+    const queries = []
+
+    const selectedEvents = pages.value[currentPage.value - 1].map(
+      (event, index) => (selected.value[index] ? event.id : undefined)
+    )
+
+    for (const uid of selectedEvents)
+      !uid ||
+        queries.push(
+          $fetch('/api/event', {
+            method: 'DELETE',
+            body: {
+              uid,
+            },
+          })
+        )
+    try {
+      await Promise.all(queries)
+    } catch (error) {
+      // TODO: #136 handle event deletion error with alerts
     }
-  })
 
-  // Send delete request to API
-  const deleteEvent = (event: any) => {
-    // TODO: Call on event API created in #111
+    dialog.value = false
+    loading.value = false
+    currentPage.value = 1
 
-    console.log(event)
+    // refresh user data
+    refresh()
   }
 </script>
 
 <template>
-  <div class="container">
-    <!-- Create new event -->
-    <VBtn
-      block
-      variant="flat"
-      color="primary"
-      @click="() => navigateTo('/admin/edit/event')"
-      class="mb-5"
-    >
-      <VIcon>mdi-plus</VIcon>
-      <span>Legg til arrangement</span>
-    </VBtn>
-
-    <!-- Event container -->
-    <VCard v-for="(event, index) in events" :key="index" elevation="3">
-      <!-- Event title -->
-      <template #title>
-        {{ event?.title }}
-      </template>
-
-      <template #text>
-        <!-- UID -->
-        <span class="card__text mb-2">
-          <VIcon color="primary">mdi-fingerprint</VIcon>
-          {{ event.id }}
-        </span>
-
-        <!-- Company -->
-        <span class="card__text mb-2">
-          <VIcon color="primary">mdi-domain</VIcon>
-          <span v-if="companies[event.companyUID]">
-            {{ companies[event.companyUID].name }}
-          </span>
-          <span class="text-primary">{{ event.companyUID }}</span>
-        </span>
-
-        <!-- Event location -->
-        <span class="card__text mb-2">
-          <VIcon color="primary">mdi-map-marker</VIcon>
-          <span>
-            {{ event.location.name }}
-            <span v-if="event.location.map !== 'null'" class="text-primary"
-              >(har link)</span
-            >
-          </span>
-        </span>
-
-        <!-- Event date -->
-        <span class="card__text mb-2">
-          <VIcon color="primary">mdi-calendar</VIcon>
-          <span>
-            {{ getNumericDayAndMonthString(event.date.start) }}
-          </span>
-          <span
-            v-if="
-              getDateFromDatetime(event.date.start) !==
-              getDateFromDatetime(event.date.end)
-            "
+  <div>
+    <div class="d-flex flex-wrap filters align-end">
+      <!-- Dates -->
+      <div>
+        <p>{{ $t('admin.events.dates') }}</p>
+        <VChipGroup v-model="selectedDates" multiple mandatory>
+          <VChip
+            v-for="(date, index) in eventDates"
+            :key="index"
+            :class="`bg-neutral-lighten-4 ${
+              Object.values(selectedDates).includes(index)
+                ? 'v-chip--selected'
+                : ''
+            }`"
           >
-            - {{ getNumericDayAndMonthString(event.date.end) }}
-          </span>
-        </span>
+            {{ getNumericDayAndMonthString(date) }}
+          </VChip>
+        </VChipGroup>
+      </div>
 
-        <!-- Event start and end time -->
-        <span class="card__text mb-2">
-          <VIcon color="primary">mdi-clock</VIcon>
-          <span>
+      <!-- Filter type -->
+      <div>
+        <p>{{ $t('admin.events.filtertypes.name') }}</p>
+        <VChipGroup v-model="filterType" mandatory>
+          <VChip
+            :class="`bg-neutral-lighten-4 ${
+              filterType === 'updated' ? 'v-chip--selected' : ''
+            }`"
+            value="updated"
+          >
+            {{ $t('admin.events.filtertypes.updated') }}
+          </VChip>
+          <VChip
+            :class="`bg-neutral-lighten-4 ${
+              filterType === 'company' ? 'v-chip--selected' : ''
+            }`"
+            value="company"
+          >
+            {{ $t('admin.events.filtertypes.company') }}
+          </VChip>
+        </VChipGroup>
+      </div>
+
+      <!-- filer order -->
+      <div>
+        <p>{{ $t('admin.events.filterorder.name') }}</p>
+        <VChipGroup v-model="filterOrder" mandatory>
+          <VChip
+            :class="`bg-neutral-lighten-4 ${
+              filterOrder === 'descending' ? 'v-chip--selected' : ''
+            }`"
+            value="descending"
+          >
+            {{ $t('admin.events.filterorder.descending') }}
+          </VChip>
+          <VChip
+            :class="`bg-neutral-lighten-4 ${
+              filterOrder === 'ascending' ? 'v-chip--selected' : ''
+            }`"
+            value="ascending"
+          >
+            {{ $t('admin.events.filterorder.ascending') }}
+          </VChip>
+        </VChipGroup>
+      </div>
+
+      <VSpacer />
+
+      <div>
+        <!-- select all button -->
+        <VBtn
+          color="primary"
+          size="large"
+          rounded="lg"
+          class="mr-2"
+          flat
+          @click="
+            () => {
+              /* @ts-ignore */
+              filteredEvents.forEach((event, index) => (selected[index] = true))
+            }
+          "
+        >
+          {{ $t('admin.events.selectall') }}
+        </VBtn>
+
+        <!-- delete users modal -->
+        <VDialog v-model="dialog" width="500">
+          <template #activator="{ props }">
+            <VBtn
+              v-bind="props"
+              :disabled="!selected.find((val) => val)"
+              color="primary"
+              size="large"
+              rounded="lg"
+              flat
+            >
+              {{ $t('admin.events.delete.selected') }}
+            </VBtn>
+          </template>
+
+          <template #default="{ isActive }">
+            <VCard rounded="lg" class="text-center pa-6">
+              <h6>{{ $t('admin.events.delete.confirmtext') }}</h6>
+
+              <div
+                class="d-flex justify-center flex-wrap mt-6"
+                style="gap: 1.5rem"
+              >
+                <VBtn
+                  size="large"
+                  variant="outlined"
+                  color="primary"
+                  :loading="loading"
+                  @click="deleteEvents"
+                >
+                  {{ $t('admin.events.delete.confirm') }}
+                </VBtn>
+                <VBtn
+                  size="large"
+                  flat
+                  color="success"
+                  @click="isActive.value = false"
+                >
+                  {{ $t('admin.events.delete.abort') }}
+                </VBtn>
+              </div>
+            </VCard>
+          </template>
+        </VDialog>
+      </div>
+    </div>
+
+    <VDivider class="mb-5 mt-2" />
+
+    <!-- Pagination -->
+    <div class="d-flex align-center justify-space-between flex-wrap">
+      <VPagination
+        v-model="currentPage"
+        :ripple="false"
+        :length="pages.length"
+        :total-visible="4"
+        density="comfortable"
+      />
+
+      <div class="d-flex align-center">
+        <p class="text-subtitle-1 mr-4 font-weight-medium">
+          {{ $t('admin.events.pagesize') }}
+        </p>
+        <VSelect
+          v-model="pageSize"
+          density="compact"
+          variant="outlined"
+          :items="pageSizes"
+          :hide-details="true"
+        />
+      </div>
+    </div>
+
+    <!-- Event table -->
+    <VTable hover style="overflow: hidden" class="mb-5">
+      <thead>
+        <tr>
+          <th>{{ $t('admin.events.attributes.select') }}</th>
+          <th>{{ $t('admin.events.attributes.title') }}</th>
+          <th>{{ $t('admin.events.attributes.company') }}</th>
+          <th>{{ $t('admin.events.attributes.date') }}</th>
+          <th>{{ $t('admin.events.attributes.time') }}</th>
+          <th>{{ $t('admin.events.attributes.location') }}</th>
+          <th class="text-center">UID</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(event, index) in pages[currentPage - 1]" :key="index">
+          <!-- select user -->
+          <td>
+            <VCheckbox v-model="selected[index]" :hide-details="true" />
+          </td>
+
+          <td>{{ event.title }}</td>
+          <td>{{ companies[event.companyUID]?.name ?? '-' }}</td>
+          <td>
+            {{ getNumericDayAndMonthString(event.date.start) }}
+          </td>
+          <td>
             {{ getHourAndMinuteStringFromString(event.date.start) }} -
             {{ getHourAndMinuteStringFromString(event.date.end) }}
-          </span>
-        </span>
+          </td>
+          <td>{{ event.location.name }}</td>
+          <td>
+            {{
+              /* @ts-ignore */
+              event.id ?? '-'
+            }}
+          </td>
 
-        <!-- Event capacity -->
-        <span class="card__text mb-2">
-          <VIcon color="primary">mdi-account</VIcon>
+          <!-- copy user id -->
+          <td>
+            <VTooltip location="top" color="primary">
+              <template #activator="{ props }">
+                <VBtn
+                  v-bind="props"
+                  size="small"
+                  variant="text"
+                  icon="mdi-content-copy"
+                  color="primary"
+                  @click="copy(event.id)"
+                />
+              </template>
 
-          <!-- Capacity count -->
-          <span v-if="event.capacity !== 'null'">
-            <span v-if="hasAttendants(event)">
-              {{ Object.values(event.attendants).length }}
-            </span>
-            <span v-else>0</span>
-            / {{ event.capacity }}
-          </span>
-          <span v-else>Ingen antallsbegrensning</span>
-        </span>
-      </template>
-
-      <!-- Event actions -->
-      <template #actions>
-        <!-- Edit event button -->
-        <VBtn
-          variant="flat"
-          @click.stop="() => navigateTo(`/admin/edit/event/${event.id}`)"
-        >
-          <VIcon>mdi-pencil</VIcon>
-          Rediger
-        </VBtn>
-
-        <!-- Delete event button -->
-        <VBtn
-          variant="tonal"
-          color="error"
-          @click.stop="() => deleteEvent(event)"
-        >
-          <VIcon>mdi-delete</VIcon>
-          Slett
-        </VBtn>
-      </template>
-    </VCard>
+              {{
+                copied && text === event.id
+                  ? $t('admin.events.copied')
+                  : event.id
+              }}
+            </VTooltip>
+          </td>
+        </tr>
+      </tbody>
+    </VTable>
   </div>
 </template>
 
 <style scoped lang="scss">
-  .container {
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem !important;
+  .filters {
+    gap: 0.5rem;
   }
-  .card {
-    padding: 1rem !important;
 
-    &__text {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      $card-gap: 0.5rem;
-      gap: $card-gap;
-
-      .link,
-      .link:visited {
-        color: rgb(var(--v-theme-neutral));
-      }
-    }
+  td,
+  th {
+    text-wrap: nowrap;
+    min-width: fit-content;
   }
-  .v-btn {
-    width: calc(50% - 4px) !important;
+
+  .v-chip {
+    transition: 0.25s;
+    user-select: none;
+  }
+  .v-chip--selected {
+    background-color: rgb(var(--v-theme-accent)) !important;
+    color: rgb(var(--v-theme-background)) !important;
   }
 </style>
