6f0ff33de5990ce701e46419460d64b245d0dce9
ipeglin <philip@eglin.no>
2023-12-20 14:58:38 +0100

    feat: adding main contact card to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <ContactCard
+      class="mx-auto"
+      :content="{
+        name: 'Ian Philip Eglin',
+        position: $t('contact.positions.IT'),
+        email: 'itansvarlig@et-dagen.no',
+        phone: '+47 123 45 678',
+        image: 'https://picsum.photos/500/300?image=60',
+      }"
+    />
+
     <CompanyGrid>
       <ContactGridCard
         :content="{
