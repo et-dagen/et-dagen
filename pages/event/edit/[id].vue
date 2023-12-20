5b261714b61f70f9572d3f2b858a191afbbff648
ipeglin <philip@eglin.no>
2023-12-20 14:27:18 +0100

    feat: adding pages for event editing

---
+++ pages/event/edit/[id].vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  definePageMeta({
+    // route and all sub routes are protected
+    protected: true,
+    // route is only accessible by admins
+    accessLevels: ['admin', 'company'],
+  })
+
+  // const route = useRoute()
+  // const { data } = await useFetch('/api/event', {
+  //   query: { eventUID: route.params.id },
+  // })
+
+  // if (!data.value) navigateTo('/event/edit')
+
+  // const event = computed(() =>
+  //   data.value
+  //     ? Object.entries(embedKeyIntoObjectValues(data.value))[0][1]
+  //     : undefined
+  // )
+</script>
+
+<template>
+  <EventEditForm :event-uid="$route.params.id" />
+</template>
