df116b0d3ade820d5b2ff8d7577dc22fa5031891
Reidar Arne Eidsvik Nerheim <raener12@gmail.com>
2023-12-23 12:37:36 +0100

    Feat: added a button on My Profile page that redirects to edit

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -6,8 +6,29 @@
 
   const authStore = useAuthStore()
   const { user } = storeToRefs(authStore)
+
+  const localePath = useLocalePath()
+  const goToEdit = () => {
+    // asume normal user
+    navigateTo(localePath(`/user/edit/${user?.id}`))
+  }
 </script>
 
 <template>
-  <div>Welcome {{ user?.name }}!</div>
+  <div>
+    <div>Welcome {{ user?.name }}!</div>
+    <!--Edit button-->
+    <VBtn
+      size="large"
+      variant="text"
+      :ripple="false"
+      :class="`rounded-lg d-flex justify-start px-3 text-background
+        ${'text-background'}`"
+      :width="'45%'"
+      :active="true"
+      @click="goToEdit"
+    >
+      Edit Account
+    </VBtn>
+  </div>
 </template>
