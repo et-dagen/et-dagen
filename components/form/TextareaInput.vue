71b28c8880e2c395108434cd21618f7dd0bb67a9
Ian Philip Eglin <philip@eglin.no>
2023-12-31 00:54:47 +0100

    feat: edit event page (#146)
    * chore: moving form components to dedicated folder
    
    * Adding edit form component, and retrieves event data
    
    * feat: fetching user and company data from database
    
    * feat: adding pages for event editing
    
    * feat: adding pages for event editing
    
    * feat: adding edit form for modifying or creating events
    
    * fix: changing prop input type
    
    * feat: adding new text area input component
    
    * feat: adding new localisation for event edit page
    
    * fix: running file parser
    
    * feat: adding composable that imbeds an objects keys into its values
    
    * fix: running lintfix
    
    * feat: adding new edit form for events
    
    * feat: adding capacity label for event edit
    
    * fix: removing console logs
    
    * feat: adding new form input type for numbers
    
    * refactor: removing requirement of limitedCapacity and eventType. also checking if capacity is null or a number less than zero
    
    * refactor: removing check for value equal to 'null'
    
    * refactor: removing left out render of event object
    
    * fix: conditionally rendering enlist button
    
    * fix: routing back to admin page on cancel or save, not to previous page
    
    * feat: adding alert support as well as separating submit functions into separate functions
    
    * feat: adding alert text for event editing
    
    * fix: you can no longer submit empty form
    
    * fix: you can no longer submit empty form
    
    * fix: fixing firebase admin sdk credential loading
    
    * chore: creating gh issue, and linking it to the code in the component

---
+++ components/form/TextareaInput.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  export interface TextareaInputContent {
+    label: string
+    hint?: string
+    rules?: string[]
+  }
+
+  defineProps({
+    content: { type: Object as PropType<TextareaInputContent>, required: true },
+  })
+</script>
+
+<template>
+  <VTextarea
+    :label="content.label"
+    type="text"
+    variant="outlined"
+    :hint="content.hint"
+  />
+</template>
