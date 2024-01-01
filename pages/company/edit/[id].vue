7737d8e0f3c2eb6056aeb8a3ab5663f2d6e64137
ipeglin <philip@eglin.no>
2024-01-01 15:13:03 +0100

    fix: making sure only admins can modify companies. this fixes security issue were a company user could change the companyType of their own company

--- pages/company/edit/[id].vue
+++ pages/company/edit/[id].vue
@@ -3,7 +3,7 @@
     // route and all sub routes are protected
     protected: true,
     // route is only accessible by admins
-    accessLevels: ['admin', 'company'],
+    accessLevels: ['admin'],
   })
 </script>
 
