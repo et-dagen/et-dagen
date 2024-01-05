2c6cacc24afb4011b6b4a65ead65798f5c0b7077
Aasmund Nørsett <aasmund.norsett@gmail.com>
2024-01-05 16:00:29 +0100

    refactor: contact page images

--- components/contact/GridCard.vue
+++ components/contact/GridCard.vue
@@ -16,7 +16,7 @@
 </script>
 
 <template>
-  <div class="card">
+  <VCard flat class="card">
     <NuxtImg class="card__image" :src="content.image" />
     <span class="text-primary semibold">{{ content.position }}</span>
     <span class="card__name bold">{{ content.name }}</span>
@@ -32,7 +32,7 @@
         </a>
       </p>
     </div>
-  </div>
+  </VCard>
 </template>
 
 <style scoped lang="scss">
@@ -50,7 +50,6 @@
     flex-direction: column;
     align-items: center;
     text-align: center;
-    aspect-ratio: 15 / 3 !important;
     max-width: 250px;
     min-width: 200px;
     padding: 12px;
