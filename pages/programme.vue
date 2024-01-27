c05aae921a78c403b44423d5ced632e15d7de9e2
Reidar Arne Eidsvik Nerheim <raener12@gmail.com>
2024-01-27 14:46:28 +0100

    Temp edit lol

--- pages/programme.vue
+++ pages/programme.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
+  <div class="conainer">
     <h4 class="text-center pt-4 pb-7">{{ $t('program.title') }}</h4>
     <ProgrammeTimeline />
   </div>
 </template>
+
+<style scoped>
+  .container {
+    max-width: 95vw !important;
+    margin: 0 auto;
+  }
+</style>
