cda4e74c7d4a12733048b11e951502ad7bb25bc1
ipeglin <philip@eglin.no>
2024-01-27 14:35:01 +0100

    refactor: time composable now respects time zone

--- composables/useTime.ts
+++ composables/useTime.ts
@@ -22,8 +22,18 @@ export const getTimeFromDatetime = (datetime: string): string => {
   return datetime.split('T')[1].replace('Z', '')
 }
 
+// get local ISO string from date compensating for timezone offset
+export const getLocalISOString = (date: Date) => {
+  const offsetMs = date.getTimezoneOffset() * 60 * 1000
+  const msLocal = date.getTime() - offsetMs
+  const dateLocal = new Date(msLocal)
+  const iso = dateLocal.toISOString()
+  const isoLocal = iso.slice(0, 19)
+  return isoLocal
+}
+
 // checking if now is within a time window of ISO strings
 export const presentWithinTimeWindow = (start: string, end: string) => {
-  const now = new Date().toISOString()
+  const now = getLocalISOString(new Date())
   return start < now && end > now
 }
--- server/utils/useTime.ts
+++ server/utils/useTime.ts
@@ -1,5 +1,15 @@
+// get local ISO string from date compensating for timezone offset
+export const getLocalISOString = (date: Date) => {
+  const offsetMs = date.getTimezoneOffset() * 60 * 1000
+  const msLocal = date.getTime() - offsetMs
+  const dateLocal = new Date(msLocal)
+  const iso = dateLocal.toISOString()
+  const isoLocal = iso.slice(0, 19)
+  return isoLocal
+}
+
 // checking if now is within a time window of ISO strings
 export const presentWithinTimeWindow = (start: string, end: string) => {
-  const now = new Date().toISOString()
+  const now = getLocalISOString(new Date())
   return start < now && end > now
 }
