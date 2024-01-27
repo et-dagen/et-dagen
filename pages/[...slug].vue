53dfd554ab7860990d0c8695a9dc218f0d18f754
Joresund <johanstore2002@gmail.com>
2024-01-27 14:41:01 +0100

    feat: translation on error 404 page

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -7,9 +7,13 @@
     <VSheet class="text-center">
       <h1 class="text-h1 pb-9">404</h1>
       <h5 class="text-h5 py-3 break-text">
-        Denne siden finnes ikke: /{{ $route.params.slug[0] }}
+        {{ $t('error.page_not_found.page_doesnt_exist') }}: /{{
+          $route.params.slug[0]
+        }}
       </h5>
-      <VBtn @click="navigateTo(localePath('/'))">Tilbake til forsiden</VBtn>
+      <VBtn @click="navigateTo(localePath('/'))">{{
+        $t('error.page_not_found.navigate_front_page')
+      }}</VBtn>
     </VSheet>
   </div>
 </template>
