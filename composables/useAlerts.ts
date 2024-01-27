48e45cf3319d6ca14260448357e63b40b7af6c04
Johan Storesund <91387450+JStoresund@users.noreply.github.com>
2024-01-27 15:49:51 +0100

    fix: event edit form giving approval if event start time is after end time (#263)
    * fix: event edit form giving approval if event start time is after end time
    
    * update: alerts take in alertType (like 'event') as a parameter
    
    * feat: error for invalid datetime
    
    * feat: translatable error messages for invalid datetime upon event creation/edit

--- composables/useAlerts.ts
+++ composables/useAlerts.ts
@@ -95,12 +95,20 @@ const errorMessageMap = new Map([
       message: 'non_admin_user',
     },
   ],
+  [
+    'Event Error: start-time-has-to-be-before-end-time',
+    {
+      source: 'event.register',
+      type: 'error',
+      message: 'end_time_before_start_time',
+    },
+  ],
 ])
 
 export type AlertType = 'error' | 'info' | 'success' | 'warning' | undefined
 export const getAlertTypeFromErrorMessage = (errorMessage: string) => {
   return (errorMessageMap.get(errorMessage)?.type || 'error') as AlertType
-}
+} // will always return "error" as AlertType
 
 export const mapErrorMessageToAlertInfo = (errorMessage: string) => {
   return errorMessageMap.get(errorMessage) || 'missing_error'
@@ -121,8 +129,14 @@ export const getAlertRouteAndType = (errorMessage: string) => {
   }
 }
 
-export const getAlertContent = (message: string, showAlert = true) => {
-  const content = getAlertRouteAndType(message)
+export const getAlertContent = (
+  errorType: string,
+  errorMessage: string,
+  showAlert = true
+) => {
+  const content = getAlertRouteAndType(
+    `${errorType} Error: ${errorMessage.toLowerCase().replaceAll(' ', '-')}`
+  )
 
   return {
     alertRoute: content.route,
