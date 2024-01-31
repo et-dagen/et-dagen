c811963b84163681ff3654e1dc8b53806c97827b
ipeglin <philip@eglin.no>
2024-01-31 20:33:54 +0100

    chore: format to string in tab

--- components/programme/Timeline.vue
+++ components/programme/Timeline.vue
@@ -64,7 +64,7 @@
             'v-tab--selected': state.selectedDate === date,
           }"
         >
-          {{ getNumericDayAndMonthString(date) }}
+          {{ getDayAndMonthString(date, true) }}
         </VTab>
       </VTabs>
     </VContainer>
