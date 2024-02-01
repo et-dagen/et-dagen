48c2fa7ae4bdc0ac11bda6ba563bc54f4a2339a5
ipeglin <philip@eglin.no>
2024-02-01 14:18:40 +0100

    feat: select input component always displays in alphabetical order

--- components/form/SelectInput.vue
+++ components/form/SelectInput.vue
@@ -8,8 +8,18 @@
   defineProps({
     content: { type: Object as PropType<SelectInputContent>, required: true },
   })
+
+  const sortedByTitle = (options: any[]) => {
+    return options.sort((a: any, b: any) => {
+      return a.title.localeCompare(b.title)
+    })
+  }
 </script>
 
 <template>
-  <VSelect :label="content.label" :items="content.options" variant="outlined" />
+  <VSelect
+    :label="content.label"
+    :items="sortedByTitle(content.options)"
+    variant="outlined"
+  />
 </template>
