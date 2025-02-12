2c30adcc6b7083557d1a4cd843aea830c5e7d2e0
Peder <piddi056@gmail.com>
2025-02-12 01:36:47 +0100

    reworked resume year filter to match other filter an avoid small bug

--- components/company/ResumeList.vue
+++ components/company/ResumeList.vue
@@ -6,54 +6,92 @@
   }>()
 
   const filterOrder = ref('ascending')
-  const availableFilters = ['1', '2', '3', '4', '5']
-  const programFilter = ref<string[] | null>(null)
+  const userYearsNames = ['1', '2', '3', '4', '5']
+  const useryears = ref([0, 1, 2, 3, 4])
+  // const availableFilters = ['1', '2', '3', '4', '5']
+  const programFilter = ref<string[]>([...userYearsNames])
+
+  // selected users
+  const selected = ref([])
 
   const currentPage = ref(1)
   const itemsPerPage = ref(10)
 
+  // reset current page and user selection
+  watch(currentPage, () => (selected.value = []))
+  watch(itemsPerPage, () => (currentPage.value = 1))
+
+  console.log(`users${users}`)
+  console.log(`lkd${programFilter}`)
+
   const filteredResumeUsers = computed(() => {
-    return users
-      .filter((user) => {
-        if (programFilter.value && programFilter.value.length > 0) {
-          return (
-            programFilter.value.includes(user.currentYear?.toString() || '') &&
-            user.resume
-          )
-        }
-        return user.resume
-      })
-      .sort((a, b) => {
-        const yearA = typeof a.currentYear === 'number' ? a.currentYear : 0
-        const yearB = typeof b.currentYear === 'number' ? b.currentYear : 0
-
-        if (filterOrder.value === 'ascending') {
-          return yearA - yearB
-        }
-
-        return yearB - yearA
-      })
-  })
-
-  const paginatedItems = computed(() => {
-    const start = (currentPage.value - 1) * itemsPerPage.value
-    const end = start + itemsPerPage.value
-
-    return filteredResumeUsers.value.slice(start, end)
-  })
-
-  // Calculate total pages
-  const totalPages = computed(() => {
-    const total = Math.ceil(
-      filteredResumeUsers.value.length / itemsPerPage.value,
+    const selectedUserYears = userYearsNames.filter((_, index) =>
+      useryears.value.includes(index),
     )
+    console.log(selectedUserYears)
 
-    if (currentPage.value > total) {
-      currentPage.value = total
-    }
+    const filteredByUserYear = users.filter((user) => {
+      if (programFilter.value && programFilter.value.length > 0) {
+        return (
+          programFilter.value.includes(user.currentYear?.toString() || '') &&
+          user.resume
+        )
+      }
+      return user.resume
+    })
 
-    return total
+    // reset user selection when filters change
+    selected.value = []
+
+    filteredByUserYear.sort((a, b) => {
+      const yearA = typeof a.currentYear === 'number' ? a.currentYear : 0
+      const yearB = typeof b.currentYear === 'number' ? b.currentYear : 0
+
+      if (filterOrder.value === 'ascending') {
+        return yearA - yearB
+      }
+
+      return yearB - yearA
+    })
+
+    return filteredByUserYear
   })
+
+  // divide the filtered user list into pages with specified page size
+  const pages = computed(() =>
+    // this was taken from stackoverflow
+    filteredResumeUsers.value.reduce((resultArray, item, index) => {
+      const chunkIndex = Math.floor(index / itemsPerPage.value)
+
+      if (!resultArray[chunkIndex]) {
+        resultArray[chunkIndex] = []
+      }
+
+      resultArray[chunkIndex].push(item)
+
+      return resultArray
+    }, [] as User[][]),
+  )
+
+  // const paginatedItems = computed(() => {
+  //   const start = (currentPage.value - 1) * itemsPerPage.value
+  //   const end = start + itemsPerPage.value
+
+  //   return filteredResumeUsers.value.slice(start, end)
+  // })
+
+  // // Calculate total pages
+  // const totalPages = computed(() => {
+  //   const total = Math.ceil(
+  //     filteredResumeUsers.value.length / itemsPerPage.value,
+  //   )
+
+  //   if (currentPage.value > total) {
+  //     currentPage.value = total
+  //   }
+
+  //   return total
+  // })
 
   const { data: companies, pending } = await useFetch<{
     [key: string]: { cvAccess: boolean }
@@ -81,15 +119,12 @@
 <template>
   <VContainer class="px-0 justify-center">
     <div class="resume-sort-buttons">
+      <!-- filter type -->
       <div>
         <p>{{ $t('company.admin.filterTypes.name') }}</p>
-        <VChipGroup v-model="programFilter" multiple>
-          <VChip
-            v-for="filter in availableFilters"
-            :key="filter"
-            :value="filter"
-          >
-            {{ filter }}
+        <VChipGroup v-model="programFilter" multiple mandatory>
+          <VChip v-for="year in userYearsNames" :key="year" :value="year">
+            {{ year }}
           </VChip>
         </VChipGroup>
       </div>
@@ -118,12 +153,16 @@
     </div>
   </VContainer>
   <v-card v-if="cvAccess" class="mx-auto w-full resume-list">
-    <v-list v-if="paginatedItems.length > 0">
-      <v-list-item v-for="user in paginatedItems" :key="user.uid" class="py-3">
+    <v-list v-if="pages.length > 0">
+      <v-list-item
+        v-for="(user, index) in pages[currentPage - 1]"
+        :key="index"
+        class="py-3"
+      >
         <v-list-item-title>{{ user.name }}</v-list-item-title>
 
         <v-list-item-subtitle class="text-high-emphasis">
-          {{ user.studyProgram }} - {{ user.currentYear }}
+          {{ user.studyProgram ?? '-' }} - {{ user.currentYear ?? '-' }}
           {{ $t('company.admin.year') }}
         </v-list-item-subtitle>
 
@@ -143,13 +182,15 @@
     <v-text v-else class="text-center py-5 text-high-emphasis">{{
       $t('company.admin.noResumes')
     }}</v-text>
-    <div class="text-center">
-      <v-pagination
-        v-model:model-value="currentPage"
-        :length="totalPages"
-        :total-visible="5"
+    <!-- pagination -->
+    <div class="d-flex align-center justify-space-between pagination-wrapper">
+      <VPagination
+        v-model="currentPage"
+        :ripple="false"
+        :length="pages.length"
+        :total-visible="4"
         density="comfortable"
-      ></v-pagination>
+      />
     </div>
   </v-card>
 </template>
