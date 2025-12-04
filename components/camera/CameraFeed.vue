a5197e3cd4f96c4ac36f4106e385c523dc36cec2
Andreas V. Jonsterhaug <andreas.vj@icloud.com>
2025-12-04 02:58:29 +0100

    camera start and stop button

--- components/camera/CameraFeed.vue
+++ components/camera/CameraFeed.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <button @click="start">Start camera</button>
     <video ref="video" autoplay playsinline muted style="display: none"></video>
     <canvas ref="canvas"></canvas>
   </div>
@@ -9,23 +8,44 @@
 <script setup lang="ts">
   import { ref, onBeforeUnmount } from 'vue'
 
-  type CallbackFunction = (_imageData: ImageData) => void
+  type CameraFeedEmits = {
+    (event: 'error', err: Error): void
+    (event: 'callbackFrame', image: ImageData): void
+  }
+
+  const emit = defineEmits<CameraFeedEmits>()
 
   const props = defineProps<{
-    callbackFunction?: CallbackFunction
     callbackInterval?: number
   }>()
 
   const video = ref<HTMLVideoElement | null>(null)
   const canvas = ref<HTMLCanvasElement | null>(null)
   let stream: MediaStream | null = null
+  const animationFrameId: number | null = null
   let width = 0
   let height = 0
 
-  const start = async () => {
-    stream = await navigator.mediaDevices.getUserMedia({
-      video: { facingMode: { ideal: 'environment' } },
-    })
+  const isRunning = ref(false)
+  const isStarting = ref(false)
+
+  async function start() {
+    if (isRunning.value || isStarting.value) {
+      console.warn('Camera has already been started')
+      return
+    }
+    isStarting.value = true
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({
+        video: { facingMode: { ideal: 'environment' } },
+      })
+    } catch (err: any) {
+      isStarting.value = false
+      if (err.name === 'NotAllowedError') {
+        emit('error', err)
+      }
+      return
+    }
 
     video.value!.srcObject = stream
     await video.value!.play()
@@ -35,24 +55,47 @@
     canvas.value!.width = width
     canvas.value!.height = height
 
-    runCallbackLoop()
+    isStarting.value = false
+    isRunning.value = true
+
+    runFrameLoop()
   }
 
-  const runCallbackLoop = () => {
+  function stop() {
+    if (stream) stream.getTracks().forEach((t) => t.stop())
+    if (animationFrameId != null) cancelAnimationFrame(animationFrameId)
+
+    isStarting.value = false
+    isRunning.value = false
+  }
+
+  defineExpose({
+    start,
+    stop,
+    isStarting,
+    isRunning,
+  })
+
+  export type CameraFeedExposed = {
+    start: () => void
+    stop: () => void
+    isRunning: Ref<boolean>
+    isStarting: Ref<boolean>
+  }
+
+  const runFrameLoop = () => {
     const ctx = canvas.value!.getContext('2d')!
     let last = 0
     const interval = props.callbackInterval ? props.callbackInterval : 0
 
     const loop = (time: number) => {
+      if (!isRunning.value) return
       ctx.drawImage(video.value!, 0, 0, width, height)
 
-      if (props.callbackFunction) {
-        if (time - last > interval) {
-          const imageData = ctx.getImageData(0, 0, width, height)
-          props.callbackFunction(imageData)
-
-          last = time
-        }
+      if (time - last > interval) {
+        const imageData = ctx.getImageData(0, 0, width, height)
+        emit('callbackFrame', imageData)
+        last = time
       }
 
       requestAnimationFrame(loop)
--- pages/admin/scan.vue
+++ pages/admin/scan.vue
@@ -1,22 +1,46 @@
-<script setup lang="ts">
-  import jsQR from 'jsqr'
-  import CameraFeed from '~/components/camera/CameraFeed.vue'
+<template>
+  <div>
+    <button @click="toggleCamera">
+      {{ camera?.isRunning ? 'Stop camera' : 'Start camera' }}
+    </button>
 
-  function cameraCallback(imageData: ImageData) {
-    const code = jsQR(imageData.data, imageData.width, imageData.height)
-    if (code) {
-      console.log(code.data)
+    <CameraFeed
+      ref="camera"
+      :callback-interval="100"
+      @callback-frame="onCallbackFrame"
+      @error="onError"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { ref } from 'vue'
+  import jsQR from 'jsqr'
+  import type { CameraFeedExposed } from '~/components/camera/CameraFeed.vue'
+
+  const camera = ref<CameraFeedExposed | null>(null)
+
+  function toggleCamera() {
+    if (!camera.value) return
+
+    if (camera.value.isRunning) {
+      camera.value.stop()
     } else {
-      console.log('No code found')
+      camera.value.start()
     }
   }
 
-  const callbackInterval = 200
-</script>
+  function onCallbackFrame(imageData: ImageData) {
+    const code = jsQR(imageData.data, imageData.width, imageData.height)
 
-<template>
-  <CameraFeed
-    :callback-function="cameraCallback"
-    :callback-interval="callbackInterval"
-  />
-</template>
+    if (code) {
+      console.log('QR detected:', code.data)
+    } else {
+      console.log('No QR code found')
+    }
+  }
+
+  function onError(err: any) {
+    alert(`Camera error: ${err}`)
+  }
+</script>
