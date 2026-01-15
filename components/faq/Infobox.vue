943100b50d55cbacf37c7d37586b72c8ab96fc48
AasmundAngard <wefling@live.no>
2026-01-15 17:08:50 +0100

    feat: Added faq/for bedrifter page, and link in nav-bar.

---
+++ components/faq/Infobox.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="infobox">
+    <button class="infobox-title" @click="isOpen = !isOpen">
+      {{ title }}
+    </button>
+
+    <div v-show="isOpen" class="infobox-content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  defineProps({
+    title: {
+      type: String,
+      required: true,
+    },
+  })
+
+  const isOpen = ref(false)
+</script>
+
+<style scoped>
+  .infobox {
+    border-bottom: 1px solid #2d2d2d;
+    background: rgba(var(--v-theme-accent), 0.08);
+    border-bottom: 1px solid rgb(195, 60, 59);
+    /* border-color: (rgb(195, 60, 59), var(--v-border-opacity)); */
+  }
+
+  .infobox-title {
+    width: 100%;
+    text-align: left;
+    padding: 12px;
+    border: none;
+    cursor: pointer;
+    transition: 200ms;
+  }
+  .infobox-title:hover {
+    color: rgb(195, 60, 59);
+  }
+
+  .infobox-content {
+    padding: 12px;
+  }
+</style>
---
+++ pages/faq/index.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+  import Infobox from '../../components/faq/Infobox.vue'
+</script>
+
+<template>
+  <div class="info-page">
+    <div class="titlewrapper my-16">
+      <h4 class="mb-6">Ofte stilte spørsmål</h4>
+    </div>
+    <div class="contentwrapper">
+      <p>Dette er bare lorem ipsum.</p>
+      <p>
+        Ved å bli medlem i E&T-dagene, får din bedrift en
+        <emph>ettertraktet</emph> mulighet til eksponering blant Gløshaugen
+        skarpeste, gjennom en standplass i Glassgården på NTNU, og mulighet for
+        å holde egne bedrift-presentasjoner, intervjuer, bla bla bla.
+      </p>
+
+      <Infobox title="Hvilke muligheter har min bedrift til profilering?"
+        ><p>
+          Vi tilbyr speedintervju, bedriftpresentasjon og gode standplasser.
+        </p>
+      </Infobox>
+      <Infobox title="Hvordan kan jeg melde på min bedrift?">
+        <p>For å melde på din bedrift, kontakt bedriftansvarlig.</p>
+      </Infobox>
+      <Infobox title="Hvor gjennomføres E&T-dagene?">
+        <p>
+          E&T-dagene gjennomføres i Glassgården, i elektrobygget. Nærmeste
+          ankomstplass for bil/taxi er på vestsiden av Hovedbygget.
+        </p>
+      </Infobox>
+      <Infobox title="Hvor kan jeg bestille ekstra utstyr?"
+        ><p>Kontakt vår logistikkansvarlig, så hjelper vi med bestilling.</p>
+      </Infobox>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .info-page {
+    max-width: 700px;
+    margin: 2rem auto;
+    /* color:#5c6873; */
+    font-family: 'Roboto', sans-serif;
+  }
+  .title {
+    font-weight: bold;
+    text-align: center;
+  }
+  .titlewrapper {
+    /* height: 5em; */
+
+    display: flex;
+
+    align-items: center;
+    justify-content: center;
+  }
+</style>
