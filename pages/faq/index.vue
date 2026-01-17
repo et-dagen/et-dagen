f4e0f7ae13d522da80b3331faba179c4d87ed693
AasmundAngard <wefling@live.no>
2026-01-17 16:10:45 +0100

    more work on page

--- pages/faq/index.vue
+++ pages/faq/index.vue
@@ -2,58 +2,86 @@
   import Infobox from '../../components/faq/Infobox.vue'
 </script>
 
+/* TODO: Add translation for the text*/
 <template>
-  <div class="info-page">
-    <div class="titlewrapper my-16">
-      <h4 class="mb-6">Ofte stilte spørsmål</h4>
-    </div>
-    <div class="contentwrapper">
-      <p>Dette er bare lorem ipsum.</p>
-      <p>
-        Ved å bli medlem i E&T-dagene, får din bedrift en
-        <emph>ettertraktet</emph> mulighet til eksponering blant Gløshaugen
-        skarpeste, gjennom en standplass i Glassgården på NTNU, og mulighet for
-        å holde egne bedrift-presentasjoner, intervjuer, bla bla bla.
-      </p>
+  <div class="container">
+    <div class="info-page">
+      <div class="titlewrapper my-16">
+        <h3>{{ $t('faq.heading') }}</h3>
+      </div>
+      <div class="contentwrapper">
+        <div class="textwrapper">
+          <p>Dette er bare lorem ipsum.</p>
+        </div>
+        <div class="textwrapper">
+          <p>
+            Ved å bli medlem av E&T-dagene, får din bedrift en
+            <emph>ettertraktet</emph> mulighet for eksponering blant Gløshaugens
+            skarpeste, gjennom en standplass i vakre Glassgården på NTNU, og
+            mulighet til å holde egne bedriftspresentasjoner, intervjuer, bla
+            bla bla.
+          </p>
+        </div>
 
-      <Infobox title="Hvilke muligheter har min bedrift til profilering?"
-        ><p>
-          Vi tilbyr speedintervju, bedriftpresentasjon og gode standplasser.
-        </p>
-      </Infobox>
-      <Infobox title="Hvordan kan jeg melde på min bedrift?">
-        <p>For å melde på din bedrift, kontakt bedriftansvarlig.</p>
-      </Infobox>
-      <Infobox title="Hvor gjennomføres E&T-dagene?">
-        <p>
-          E&T-dagene gjennomføres i Glassgården, i elektrobygget. Nærmeste
-          ankomstplass for bil/taxi er på vestsiden av Hovedbygget.
-        </p>
-      </Infobox>
-      <Infobox title="Hvor kan jeg bestille ekstra utstyr?"
-        ><p>Kontakt vår logistikkansvarlig, så hjelper vi med bestilling.</p>
-      </Infobox>
+        <div class="subheading">
+          <h4>{{ $t('faq.subheading') }}</h4>
+        </div>
+
+        <Infobox title="Hvilke muligheter har min bedrift til profilering?"
+          ><p>
+            Vi tilbyr speedintervju, bedriftpresentasjon og gode standplasser.
+          </p>
+        </Infobox>
+        <Infobox title="Hvordan kan jeg melde på min bedrift?">
+          <p>For å melde på din bedrift, kontakt vår bedriftsansvarlig.</p>
+        </Infobox>
+        <Infobox title="Hvor gjennomføres E&T-dagene?">
+          <p>
+            E&T-dagene gjennomføres i Glassgården, i elektrobygget. Nærmeste
+            ankomstplass for bil/taxi er på vestsiden av Hovedbygget.
+          </p>
+        </Infobox>
+        <Infobox title="Hvor kan jeg bestille utstyr?"
+          ><p>Kontakt vår logistikkansvarlig, så hjelper vi med bestilling.</p>
+        </Infobox>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
+  .container {
+    margin: 0 auto;
+    padding: 0 30px;
+  }
+
   .info-page {
-    max-width: 700px;
-    margin: 2rem auto;
-    /* color:#5c6873; */
     font-family: 'Roboto', sans-serif;
+  }
+  @media (min-width: 768px) {
+    .container {
+      width: 750px;
+    }
+  }
+  @media (min-width: 960) {
+    .container {
+      width: 930px;
+    }
   }
   .title {
     font-weight: bold;
     text-align: center;
   }
+  .subheading {
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
   .titlewrapper {
-    /* height: 5em; */
-
     display: flex;
-
     align-items: center;
     justify-content: center;
+  }
+  .textwrapper {
+    padding-bottom: 15px;
   }
 </style>
