5cd7072856f6faa1bc18a47b22440c68c722f559
Philip Eglin <philip@eglin.no>
2026-08-09 18:47:54 +0200

    docs: adding documentation comments

--- domain/user.ts
+++ domain/user.ts
@@ -5,21 +5,50 @@ export type Role = 'admin' | 'coordinator' | 'company' | 'user'
 export interface BaseUser {
   uid: string
   email: string
-  name: string
   roles: Role[]
   updated: number
   dietaryRestrictions?: string[]
 }
 
-export interface StudentUser extends BaseUser {
-  year: number
+/**
+ * Some users should have names
+ */
+export interface UserWithName {
+  name: string
+}
+
+/**
+ * University student users
+ *
+ * @remarks
+ * For keeping track of programme attendence and progression for limited events.
+ * Student users have to enlist for events.
+ */
+export interface StudentUser extends BaseUser, UserWithName {
   programme: StudyProgramme
+  year: number
 }
 
-export interface CoordinatorUser extends StudentUser {
-  companyUID: string[]
+/**
+ * Company coordinators from E&T-dagene
+ *
+ * @remarks
+ * Coordinators have the ability to modify events, jobs listings and details on their respective companies,
+ * while keeping the ability to registering and act as normal users on the side.
+ * Automatically signed up to events hosted by companies under their responsibility,
+ * not counted as attendants but hosts.
+ */
+export interface CoordinatorUser extends StudentUser, UserWithName {
+  companyUID: string[] // company coordinators can have multiple companies
 }
 
+/**
+ * Non-student user working at an attending company
+ *
+ * @remarks
+ * May change details of it's own company, events and job listings.
+ * Cannot sign up for events, but are counted in counter for hosting.
+ */
 export interface CompanyUser extends BaseUser {
   companyUID: string
 }
